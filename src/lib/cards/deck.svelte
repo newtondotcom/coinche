8adf75605b660fdca7752d8f37e30b543509fc53
newtondotcom <asphalt9fr@gmail.com>
2023-12-01 11:56:34 +0100

    player deck

---
+++ src/lib/cards/deck.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+    import Card from "$lib/cards/card.svelte";
+    export let hand : IPlayCard[];
+
+
+</script>
+
+<div class="flex flex-row absolute bottom-0 justify-center w-full">
+{#each hand as card}
+    <Card suite={card.suite} value={card.value} />
+{/each}
+</div>
