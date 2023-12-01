d6f09361edacec959cc6b7fa4f64c10e41b4f4d0
newtondotcom <asphalt9fr@gmail.com>
2023-12-01 11:56:45 +0100

    other players deck

---
+++ src/lib/cards/fake.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+	import { maxCardWidth } from '$lib/constants';
+
+	export let side: "left" | "right" | "top";
+	export let qqty: number;
+
+	let tabGen = (qty: number) => {
+		let tab = [];
+		for (let i = 0; i < qty; i++) {
+			tab.push(i);
+		}
+		return tab;
+	}
+</script>
+
+<div
+	class="{side === 'left' ? 'flex flex-col absolute left-0 h-full justify-center items-center' : side === 'right' ? 'flex flex-col absolute right-0 h-full justify-center items-center' : 'flex flex-row absolute top-20 justify-center w-full shrink'}"
+>
+{#each tabGen(qqty) as i}
+	<img
+		src="/dos.webp"
+		alt="dos"
+		style="max-width:{maxCardWidth + 20}px; height: auto;"
+		class="{side === 'top' ? '-rotate-90' : ''}"
+	/>
+{/each}
+</div>
