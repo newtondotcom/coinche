6fa231aedf1ec7d7a807a6790b4090f2bd36044c
newtondotcom <asphalt9fr@gmail.com>
2023-12-01 11:09:48 +0100

    new version with deps

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,11 @@
+<script lang="ts">
+    import Card from "$lib/cards/card.svelte";
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+
+<Card suite="hearts" value="A" />
+<Card suite="hearts" value="2" />
+<Card suite="hearts" value="A" />
