cfa8a03ad49080557bae78bcbf3e79280c603f96
newtondotcom <asphalt9fr@gmail.com>
2023-12-01 11:57:02 +0100

    current river deck

---
+++ src/lib/cards/river.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+    import Card from "$lib/cards/card.svelte";
+    export let hand: IPlayCard[];
+</script>
+
+<div class="flex flex-row justify-center items-center h-screen w-screen">
+    {#each hand as card}
+        <Card suite={card.suite} value={card.value} />
+    {/each}
+</div>
