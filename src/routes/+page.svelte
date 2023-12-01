a4d359567ca8df1400cac9469b210e0090645b62
newtondotcom <asphalt9fr@gmail.com>
2023-12-01 11:55:46 +0100

    main page for test

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,31 @@
 <script lang="ts">
-    import Card from "$lib/cards/card.svelte";
+    import Deck from "$lib/cards/deck.svelte";
+    import Fake from "$lib/cards/fake.svelte";
+    import River from "$lib/cards/river.svelte";
+
+    function generateRandomDeck(): IPlayCard[] {
+        const suites: CardSuite[] = ['diamonds', 'clubs', 'hearts', 'spades'];
+        const values: CardValue[] = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'];
+        const deck: IPlayCard[] = [];
+
+        while (deck.length < 8) {
+            const randomSuite: CardSuite = suites[Math.floor(Math.random() * suites.length)];
+            const randomValue: CardValue = values[Math.floor(Math.random() * values.length)];
+            const key: string = Math.random().toString();
+            const valueNum: number = values.indexOf(randomValue) + 1;
+            const isDuplicate = deck.some((card) => card.suite === randomSuite && card.value === randomValue);
+            if (!isDuplicate) {
+                deck.push({ suite: randomSuite, value: randomValue, key, valueNum });
+            }
+        }
+        return deck;
+    }
+    let hand = generateRandomDeck();
 </script>
 
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<Deck {hand} />
+<Fake side="top" qqty={8} />
+<Fake side="left" qqty={8} />
+<Fake side="right" qqty={8} />
+<River {hand} />
 
-
-<Card suite="hearts" value="A" />
-<Card suite="hearts" value="2" />
-<Card suite="hearts" value="A" />
