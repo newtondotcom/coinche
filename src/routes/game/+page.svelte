5580ab7cd93fb4e312c8d63185823770d77817f1
newtondotcom <asphalt9fr@gmail.com>
2023-12-02 19:30:55 +0100

    componentized+Filter

--- src/routes/game/+page.svelte
+++ src/routes/game/+page.svelte
@@ -2,61 +2,18 @@
     import Deck from "$lib/cards/deck.svelte";
     import Fake from "$lib/cards/fake.svelte";
     import River from "$lib/cards/river.svelte";
-    import Modal from "$lib/ui/Modal.svelte";
+    import Filter from "$lib/ui/Filter.svelte";
+    import { generateRandomDeck, generateRiver } from "$lib/utils/deck";
 
-
-    let showModal = false
-    const handleToggleModal = () => {
-        showModal = !showModal
-    }
-
-    function generateRandomDeck(): IPlayCard[] {
-        const suites: CardSuite[] = ['diamonds', 'clubs', 'hearts', 'spades'];
-        const values: CardValue[] = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'];
-        const deck: IPlayCard[] = [];
-
-        while (deck.length < 8) {
-            const randomSuite: CardSuite = suites[Math.floor(Math.random() * suites.length)];
-            const randomValue: CardValue = values[Math.floor(Math.random() * values.length)];
-            const key: string = Math.random().toString();
-            const valueNum: number = values.indexOf(randomValue) + 1;
-            const isDuplicate = deck.some((card) => card.suite === randomSuite && card.value === randomValue);
-            if (!isDuplicate) {
-                deck.push({ suite: randomSuite, value: randomValue, key, valueNum });
-            }
-        }
-        return deck;
-    }
     let hand = generateRandomDeck();
+    let river = generateRiver();
 </script>
 
-<Deck {hand} />
-<Fake side="top" qqty={8} />
-<Fake side="left" qqty={8} />
-<Fake side="right" qqty={8} />
-<River {hand} />
-
-<a
-class="rounded-md bg-teal-600 px-5 py-2.5 text-sm font-medium text-white shadow"
-href="/"
->
-Créer une partie
-</a>
-
-<a
-class="hidden rounded-md bg-gray-100 px-5 py-2.5 text-sm font-medium text-teal-600 transition hover:text-teal-600/75 sm:block"
-href="/"
->
-Rejoindre une partie
-</a>
-
-<button on:click={() => handleToggleModal()}>Open modal</button>
-<Modal
-  title="Edit your details"
-  open={showModal}
-  on:close={() => handleToggleModal()}
->
-  <svelte:fragment slot="body">
-    This is content inside my modal! 👋
-  </svelte:fragment>
-</Modal>
+<div id="main">
+  <Deck {hand} />
+  <Fake side="top" qqty={8} />
+  <Fake side="left" qqty={8} />
+  <Fake side="right" qqty={8} />
+  <River {hand} />
+  <Filter ordreAtout={true} ordreValeur={true} />
+</div>
