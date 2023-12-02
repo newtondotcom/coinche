14b98faf2f80d649e5f4e937060ff4224396976f
newtondotcom <asphalt9fr@gmail.com>
2023-12-02 18:31:23 +0100

    modal to join a game + loading symbols

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,9 +4,11 @@
 
     let showModalCreate = false
     const handleToggleModalCreate = () => {
+        handleToggleLoading()
         showModalCreate = !showModalCreate
+        handleToggleLoading()
     }
-    let showModalJoin = true
+    let showModalJoin = false
     const handleToggleModalJoin = () => {
         showModalJoin = !showModalJoin
     }
@@ -17,6 +19,17 @@
             copycode = !copycode
         }, 1000)
     }
+    let loading = false
+    const handleToggleLoading = () => {
+        loading = !loading
+    }
+
+    async function joinParty() {
+        handleToggleLoading()
+        await new Promise(r => setTimeout(r, 2000));
+        handleToggleLoading()
+    }
+
 </script>
 
 <div class="h-full flex flex-col align-middle gap-4  w-full">
@@ -26,7 +39,11 @@
         class="rounded-md bg-teal-600 px-5 py-2.5 text-sm font-medium text-white shadow"
         on:click={() => handleToggleModalCreate()}
         >
+        {#if !loading}
         Créer une partie
+        {:else}
+        <svg class="animate-spin h-5 w-5 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"><circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="1.5"></circle><path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3.169 1.952 1.952 4.538 3.13 7.35 3.13V17z"></path></svg>
+        {/if}
         </button>
         <button
         class="hidden rounded-md bg-gray-100 px-5 py-2.5 text-sm font-medium text-teal-600 transition hover:text-teal-600/75 sm:block"
@@ -43,6 +60,7 @@
   on:close={() => handleToggleModalCreate()}
 >
   <svelte:fragment slot="body">
+    Partage le code avec tes amis pour qu'ils puissent rejoindre ta partie !
     <div class="relative flex justify-center flex-box">    
       <div>
       <input
@@ -71,19 +89,18 @@
 </Modal>
 
 <Modal
-  title="Entre le nom de la partie 👋"
+  title="Entre le code de la partie 👋"
   open={showModalJoin}
   on:close={() => handleToggleModalJoin()}
 >
   <svelte:fragment slot="body">
+    <div class="flex flex-row align-middle justify-center">
     <div class="relative">
-      <label for="UserEmail" class="sr-only"> Email </label>
-    
       <input
         type="email"
         id="UserEmail"
-        placeholder="flea@rhcp.com"
-        class="w-full rounded-md border-gray-200 pe-10 shadow-sm sm:text-sm"
+        placeholder="123456"
+        class="w-full rounded-md border-gray-200 pe-10 px-5 py-2.5 shadow-sm sm:text-sm"
       />
     
       <span
@@ -98,5 +115,19 @@
         </svg>
       </span>
     </div>
+    <div>
+    <button
+    class="ml-8 rounded-md bg-teal-600 px-5 py-2.5 text-sm font-medium text-white shadow text-center"
+    on:click={() => joinParty()}
+    >
+    {#if !loading}
+    Rejoindre
+    {:else}
+    <svg class="animate-spin h-5 w-5 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"><circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="1.5"></circle><path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3.169 1.952 1.952 4.538 3.13 7.35 3.13V17z"></path></svg>
+    {/if}
+
+    </button>
+    </div>
+  </div>
   </svelte:fragment>
 </Modal>
