65e36ce27c01ab7b8d78432ddd9b6b5a0549d602
newtondotcom <asphalt9fr@gmail.com>
2023-12-18 17:15:38 +0100

    text center

--- src/lib/ui/Score.svelte
+++ src/lib/ui/Score.svelte
@@ -8,35 +8,35 @@
       <ul class="flex items-center gap-6 text-sm">
         <li>
           <div
-            class="transition hover:text-gray-500/75 w-24 bg-teal-400 rounded-xl px-4 py-2 text-white"
+            class="transition hover:text-gray-500/75 w-24 bg-teal-400 rounded-xl px-4 py-2 text-white text-center"
           >
-            Atout <br> <span class="text-teal-600">♥</span>
+            Atout <span class="text-teal-600">♥</span>
         </div>
         </li>
         <li>
           <div
-            class="text-gray-500 transition hover:text-gray-500/75"
+            class="text-gray-500 transition hover:text-gray-500/75 text-center"
           >
             Dernier pli <br> <span class="text-teal-600">9♥ J♥ 10♥ 7♥</span>
         </div>
         </li>
         <li>
           <div
-            class="text-gray-500 transition hover:text-gray-500/75"
+            class="text-gray-500 transition hover:text-gray-500/75 text-center"
           >
             Annonce <br> <span class="text-teal-600">100♥</span>
         </div>
         </li>
         <li>
           <div
-            class="text-gray-500 transition hover:text-gray-500/75"
+            class="text-gray-500 transition hover:text-gray-500/75 text-center"
           >
             Points de ta team <br> <span class="text-teal-600">320</span>
         </div>
         </li>
         <li>
           <div
-            class="text-gray-500 transition hover:text-gray-500/75"
+            class="text-gray-500 transition hover:text-gray-500/75 text-center"
           >
           Points adverses <br> <span class="text-teal-600">280</span>
         </div>
