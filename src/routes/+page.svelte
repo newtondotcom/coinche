ae702ee04aca9bc9a2b5e9d9b4b8f77c3da0aa22
newtondotcom <asphalt9fr@gmail.com>
2023-12-18 17:14:11 +0100

    copy to clipboard working

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,6 +2,8 @@
     import Modal from "$lib/ui/Modal.svelte";
     import Subtitle from "$lib/ui/Subtitle.svelte";
 
+    let code = "123456"
+
     let showModalCreate = false
     const handleToggleModalCreate = () => {
         handleToggleLoading()
@@ -14,6 +16,7 @@
     }
     let copycode = false
     const handleToggleCopyCode = () => {
+        navigator.clipboard.writeText(code);
         copycode = !copycode
         setTimeout(() => {
             copycode = !copycode
@@ -32,171 +35,154 @@
 
 </script>
 
-<div class="h-full flex flex-col align-middle gap-4  w-full">
-    <Subtitle title="Coinche moi la" subtitle="beaucoup" />
-    <div class="sm:flex sm:gap-4 flex-row align-middle justify-center items-center gap-4  w-full">
-        <button
-        class="rounded-md bg-teal-600 px-5 py-2.5 text-sm font-medium text-white shadow"
-        on:click={() => handleToggleModalCreate()}
-        >
-        {#if !loading}
-        Créer une partie
-        {:else}
-        <svg class="animate-spin h-5 w-5 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"><circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="1.5"></circle><path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3.169 1.952 1.952 4.538 3.13 7.35 3.13V17z"></path></svg>
-        {/if}
-        </button>
-        <button
-        class="hidden rounded-md bg-gray-100 px-5 py-2.5 text-sm font-medium text-teal-600 transition hover:text-teal-600/75 sm:block"
-        on:click={() => handleToggleModalJoin()}
-        >
-        Rejoindre une partie
-        </button>
-    </div>
-</div>
-
-<Modal
-  title="Voici le code de ta partie 👋"
-  open={showModalCreate}
-  on:close={() => handleToggleModalCreate()}
->
-  <svelte:fragment slot="body">
-    Partage le code avec tes amis pour qu'ils puissent rejoindre ta partie !
-    <div class="relative flex justify-center flex-box">    
-      <div>
-      <input
-        type="email"
-        id="UserEmail"
-        placeholder="flea@rhcp.com"
-        value="123456"
-        class="w-full rounded-md border-gray-200 pe-10 shadow-sm sm:text-sm text-2xl text-center p-4"
-        disabled
-      />    
-    <button
-      class="absolute inset-y-0 end-0 grid w-10 place-content-center text-gray-500"
-      on:click={() => handleToggleCopyCode()}
-    >
-    {#if !copycode}
-    <svg class="w-5 h-5 ml-1.5 stroke-current" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" d="M9 12h3.75M9 15h3.75M9 18h3.75m3 .75H18a2.25 2.25 0 002.25-2.25V6.108c0-1.135-.845-2.098-1.976-2.192a48.424 48.424 0 00-1.123-.08m-5.801 0c-.065.21-.1.433-.1.664 0 .414.336.75.75.75h4.5a.75.75 0 00.75-.75 2.25 2.25 0 00-.1-.664m-5.8 0A2.251 2.251 0 0113.5 2.25H15c1.012 0 1.867.668 2.15 1.586m-5.8 0c-.376.023-.75.05-1.124.08C9.095 4.01 8.25 4.973 8.25 6.108V8.25m0 0H4.875c-.621 0-1.125.504-1.125 1.125v11.25c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V9.375c0-.621-.504-1.125-1.125-1.125H8.25zM6.75 12h.008v.008H6.75V12zm0 3h.008v.008H6.75V15zm0 3h.008v.008H6.75V18z" /></svg>   
-    {:else}               
-    <svg class="w-5 h-5 ml-1.5 text-green-500 stroke-current" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" x-cloak><path stroke-linecap="round" stroke-linejoin="round" d="M11.35 3.836c-.065.21-.1.433-.1.664 0 .414.336.75.75.75h4.5a.75.75 0 00.75-.75 2.25 2.25 0 00-.1-.664m-5.8 0A2.251 2.251 0 0113.5 2.25H15c1.012 0 1.867.668 2.15 1.586m-5.8 0c-.376.023-.75.05-1.124.08C9.095 4.01 8.25 4.973 8.25 6.108V8.25m8.9-4.414c.376.023.75.05 1.124.08 1.131.094 1.976 1.057 1.976 2.192V16.5A2.25 2.25 0 0118 18.75h-2.25m-7.5-10.5H4.875c-.621 0-1.125.504-1.125 1.125v11.25c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V18.75m-7.5-10.5h6.375c.621 0 1.125.504 1.125 1.125v9.375m-8.25-3l1.5 1.5 3-3.75" /></svg>
-    {/if}
-  </button>
-    </div>
-    
-
-    </div>
-  </svelte:fragment>
-</Modal>
-
-<Modal
-  title="Entre le code de la partie 👋"
-  open={showModalJoin}
-  on:close={() => handleToggleModalJoin()}
->
-  <svelte:fragment slot="body">
-    <div class="flex flex-row align-middle justify-center">
-    <div class="relative">
-      <input
-        type="email"
-        id="UserEmail"
-        placeholder="123456"
-        class="w-full rounded-md border-gray-200 pe-10 px-5 py-2.5 shadow-sm sm:text-sm"
-      />
-    
-      <span
-        class="pointer-events-none absolute inset-y-0 end-0 grid w-10 place-content-center text-gray-500"
-      >
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="h-4 w-4">
-          <path
-            fill-rule="evenodd"
-            d="M5.404 14.596A6.5 6.5 0 1116.5 10a1.25 1.25 0 01-2.5 0 4 4 0 10-.571 2.06A2.75 2.75 0 0018 10a8 8 0 10-2.343 5.657.75.75 0 00-1.06-1.06 6.5 6.5 0 01-9.193 0zM10 7.5a2.5 2.5 0 100 5 2.5 2.5 0 000-5z"
-            clip-rule="evenodd"
-          />
-        </svg>
-      </span>
-    </div>
-    <div>
-    <button
-    class="ml-8 rounded-md bg-teal-600 px-5 py-2.5 text-sm font-medium text-white shadow text-center"
-    on:click={() => joinParty()}
-    >
-    {#if !loading}
-    Rejoindre
-    {:else}
-    <svg class="animate-spin h-5 w-5 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"><circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="1.5"></circle><path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3.169 1.952 1.952 4.538 3.13 7.35 3.13V17z"></path></svg>
-    {/if}
-
-    </button>
-    </div>
+<div class="min-h-screen">
+  <div class="h-full flex flex-col align-middle gap-4  w-full">
+      <Subtitle title="Coinche moi la" subtitle="beaucoup" />
+      <div class="sm:flex sm:gap-4 flex-row align-middle justify-center items-center gap-4  w-full">
+          <button
+          class="rounded-md bg-teal-600 px-5 py-2.5 text-sm font-medium text-white shadow"
+          on:click={() => handleToggleModalCreate()}
+          >
+          {#if !loading}
+          Créer une partie
+          {:else}
+          <svg class="animate-spin h-5 w-5 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"><circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="1.5"></circle><path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3.169 1.952 1.952 4.538 3.13 7.35 3.13V17z"></path></svg>
+          {/if}
+          </button>
+          <button
+          class="hidden rounded-md bg-gray-100 px-5 py-2.5 text-sm font-medium text-teal-600 transition hover:text-teal-600/75 sm:block"
+          on:click={() => handleToggleModalJoin()}
+          >
+          Rejoindre une partie
+          </button>
+      </div>
   </div>
-  </svelte:fragment>
-</Modal>
 
-<div class="relative bottom-4 mx-auto text-center">
-  <ul class="mt-12 flex justify-center gap-6 md:gap-8">
-    <li>
-      <a
-        href="https://github.com/newtondotcom/locapark"
-        rel="noreferrer"
-        target="_blank"
-        class="text-gray-700 transition hover:text-gray-700/75"
+  <Modal
+    title="Voici le code de ta partie 👋"
+    open={showModalCreate}
+    on:close={() => handleToggleModalCreate()}
+  >
+    <svelte:fragment slot="body">
+      Partage le code avec tes amis pour qu'ils puissent rejoindre ta partie !
+      <div class="relative flex justify-center flex-box">    
+        <div>
+        <input
+          type="email"
+          id="UserEmail"
+          placeholder="flea@rhcp.com"
+          value="{code}"
+          class="w-full rounded-md border-gray-200 pe-10 shadow-sm sm:text-sm text-2xl text-center p-4"
+          disabled
+        />    
+      <button
+        class="absolute inset-y-0 end-0 grid w-10 place-content-center text-gray-500"
+        on:click={() => handleToggleCopyCode()}
       >
-        <span class="sr-only">GitHub</span>
-        <svg
-          class="xl:h-7 xl:w-7 sm:w-20 sm:h-20"
-          fill="currentColor"
-          viewBox="0 0 24 24"
-          aria-hidden="true"
+      {#if !copycode}
+      <svg class="w-5 h-5 ml-1.5 stroke-current" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" d="M9 12h3.75M9 15h3.75M9 18h3.75m3 .75H18a2.25 2.25 0 002.25-2.25V6.108c0-1.135-.845-2.098-1.976-2.192a48.424 48.424 0 00-1.123-.08m-5.801 0c-.065.21-.1.433-.1.664 0 .414.336.75.75.75h4.5a.75.75 0 00.75-.75 2.25 2.25 0 00-.1-.664m-5.8 0A2.251 2.251 0 0113.5 2.25H15c1.012 0 1.867.668 2.15 1.586m-5.8 0c-.376.023-.75.05-1.124.08C9.095 4.01 8.25 4.973 8.25 6.108V8.25m0 0H4.875c-.621 0-1.125.504-1.125 1.125v11.25c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V9.375c0-.621-.504-1.125-1.125-1.125H8.25zM6.75 12h.008v.008H6.75V12zm0 3h.008v.008H6.75V15zm0 3h.008v.008H6.75V18z" /></svg>   
+      {:else}               
+      <svg class="w-5 h-5 ml-1.5 text-green-500 stroke-current" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" d="M11.35 3.836c-.065.21-.1.433-.1.664 0 .414.336.75.75.75h4.5a.75.75 0 00.75-.75 2.25 2.25 0 00-.1-.664m-5.8 0A2.251 2.251 0 0113.5 2.25H15c1.012 0 1.867.668 2.15 1.586m-5.8 0c-.376.023-.75.05-1.124.08C9.095 4.01 8.25 4.973 8.25 6.108V8.25m8.9-4.414c.376.023.75.05 1.124.08 1.131.094 1.976 1.057 1.976 2.192V16.5A2.25 2.25 0 0118 18.75h-2.25m-7.5-10.5H4.875c-.621 0-1.125.504-1.125 1.125v11.25c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V18.75m-7.5-10.5h6.375c.621 0 1.125.504 1.125 1.125v9.375m-8.25-3l1.5 1.5 3-3.75" /></svg>
+      {/if}
+    </button>
+      </div>
+      
+
+      </div>
+    </svelte:fragment>
+  </Modal>
+
+  <Modal
+    title="Entre le code de la partie 👋"
+    open={showModalJoin}
+    on:close={() => handleToggleModalJoin()}
+  >
+    <svelte:fragment slot="body">
+      <div class="flex flex-row align-middle justify-center">
+      <div class="relative">
+        <input
+          type="email"
+          id="UserEmail"
+          placeholder="123456"
+          class="w-full rounded-md border-gray-200 pe-10 px-5 py-2.5 shadow-sm sm:text-sm"
+        />
+      
+        <span
+          class="pointer-events-none absolute inset-y-0 end-0 grid w-10 place-content-center text-gray-500"
         >
-          <path
-            fill-rule="evenodd"
-            d="M12 2C6.477 2 2 6.484 2 12.017c0 4.425 2.865 8.18 6.839 9.504.5.092.682-.217.682-.483 0-.237-.008-.868-.013-1.703-2.782.605-3.369-1.343-3.369-1.343-.454-1.158-1.11-1.466-1.11-1.466-.908-.62.069-.608.069-.608 1.003.07 1.531 1.032 1.531 1.032.892 1.53 2.341 1.088 2.91.832.092-.647.35-1.088.636-1.338-2.22-.253-4.555-1.113-4.555-4.951 0-1.093.39-1.988 1.029-2.688-.103-.253-.446-1.272.098-2.65 0 0 .84-.27 2.75 1.026A9.564 9.564 0 0112 6.844c.85.004 1.705.115 2.504.337 1.909-1.296 2.747-1.027 2.747-1.027.546 1.379.202 2.398.1 2.651.64.7 1.028 1.595 1.028 2.688 0 3.848-2.339 4.695-4.566 4.943.359.309.678.92.678 1.855 0 1.338-.012 2.419-.012 2.747 0 .268.18.58.688.482A10.019 10.019 0 0022 12.017C22 6.484 17.522 2 12 2z"
-            clip-rule="evenodd"
-          />
-        </svg>
-      </a>
-    </li>
-
-    <li>
-      <a
-        href="https://www.instagram.com/newton.creations/"
-        rel="noreferrer"
-        target="_blank"
-        class="text-gray-700 transition hover:text-gray-700/75"
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="h-4 w-4">
+            <path
+              fill-rule="evenodd"
+              d="M5.404 14.596A6.5 6.5 0 1116.5 10a1.25 1.25 0 01-2.5 0 4 4 0 10-.571 2.06A2.75 2.75 0 0018 10a8 8 0 10-2.343 5.657.75.75 0 00-1.06-1.06 6.5 6.5 0 01-9.193 0zM10 7.5a2.5 2.5 0 100 5 2.5 2.5 0 000-5z"
+              clip-rule="evenodd"
+            />
+          </svg>
+        </span>
+      </div>
+      <div>
+      <button
+      class="ml-8 rounded-md bg-teal-600 px-5 py-2.5 text-sm font-medium text-white shadow text-center"
+      on:click={() => joinParty()}
       >
-        <span class="sr-only">Instagram</span>
-        <svg
-          class="xl:h-7 xl:w-7 sm:w-20 sm:h-20"
-          fill="currentColor"
-          viewBox="0 0 24 24"
-          aria-hidden="true"
+      {#if !loading}
+      Rejoindre
+      {:else}
+      <svg class="animate-spin h-5 w-5 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"><circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="1.5"></circle><path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3.169 1.952 1.952 4.538 3.13 7.35 3.13V17z"></path></svg>
+      {/if}
+
+      </button>
+      </div>
+    </div>
+    </svelte:fragment>
+  </Modal>
+
+  <div class="relative bottom-4 mx-auto text-center">
+    <ul class="mt-12 flex justify-center gap-6 md:gap-8">
+      <li>
+        <a
+          href="https://github.com/newtondotcom/locapark"
+          rel="noreferrer"
+          target="_blank"
+          class="text-gray-700 transition hover:text-gray-700/75"
         >
-          <path
-            fill-rule="evenodd"
-            d="M12.315 2c2.43 0 2.784.013 3.808.06 1.064.049 1.791.218 2.427.465a4.902 4.902 0 011.772 1.153 4.902 4.902 0 011.153 1.772c.247.636.416 1.363.465 2.427.048 1.067.06 1.407.06 4.123v.08c0 2.643-.012 2.987-.06 4.043-.049 1.064-.218 1.791-.465 2.427a4.902 4.902 0 01-1.153 1.772 4.902 4.902 0 01-1.772 1.153c-.636.247-1.363.416-2.427.465-1.067.048-1.407.06-4.123.06h-.08c-2.643 0-2.987-.012-4.043-.06-1.064-.049-1.791-.218-2.427-.465a4.902 4.902 0 01-1.772-1.153 4.902 4.902 0 01-1.153-1.772c-.247-.636-.416-1.363-.465-2.427-.047-1.024-.06-1.379-.06-3.808v-.63c0-2.43.013-2.784.06-3.808.049-1.064.218-1.791.465-2.427a4.902 4.902 0 011.153-1.772A4.902 4.902 0 015.45 2.525c.636-.247 1.363-.416 2.427-.465C8.901 2.013 9.256 2 11.685 2h.63zm-.081 1.802h-.468c-2.456 0-2.784.011-3.807.058-.975.045-1.504.207-1.857.344-.467.182-.8.398-1.15.748-.35.35-.566.683-.748 1.15-.137.353-.3.882-.344 1.857-.047 1.023-.058 1.351-.058 3.807v.468c0 2.456.011 2.784.058 3.807.045.975.207 1.504.344 1.857.182.466.399.8.748 1.15.35.35.683.566 1.15.748.353.137.882.3 1.857.344 1.054.048 1.37.058 4.041.058h.08c2.597 0 2.917-.01 3.96-.058.976-.045 1.505-.207 1.858-.344.466-.182.8-.398 1.15-.748.35-.35.566-.683.748-1.15.137-.353.3-.882.344-1.857.048-1.055.058-1.37.058-4.041v-.08c0-2.597-.01-2.917-.058-3.96-.045-.976-.207-1.505-.344-1.858a3.097 3.097 0 00-.748-1.15 3.098 3.098 0 00-1.15-.748c-.353-.137-.882-.3-1.857-.344-1.023-.047-1.351-.058-3.807-.058zM12 6.865a5.135 5.135 0 110 10.27 5.135 5.135 0 010-10.27zm0 1.802a3.333 3.333 0 100 6.666 3.333 3.333 0 000-6.666zm5.338-3.205a1.2 1.2 0 110 2.4 1.2 1.2 0 010-2.4z"
-            clip-rule="evenodd"
-          />
-        </svg>
-      </a>
-    </li>
+          <span class="sr-only">GitHub</span>
+          <svg
+            class="lg:h-7 lg:w-7 sm:w-20 sm:h-20"
+            fill="currentColor"
+            viewBox="0 0 24 24"
+            aria-hidden="true"
+          >
+            <path
+              fill-rule="evenodd"
+              d="M12 2C6.477 2 2 6.484 2 12.017c0 4.425 2.865 8.18 6.839 9.504.5.092.682-.217.682-.483 0-.237-.008-.868-.013-1.703-2.782.605-3.369-1.343-3.369-1.343-.454-1.158-1.11-1.466-1.11-1.466-.908-.62.069-.608.069-.608 1.003.07 1.531 1.032 1.531 1.032.892 1.53 2.341 1.088 2.91.832.092-.647.35-1.088.636-1.338-2.22-.253-4.555-1.113-4.555-4.951 0-1.093.39-1.988 1.029-2.688-.103-.253-.446-1.272.098-2.65 0 0 .84-.27 2.75 1.026A9.564 9.564 0 0112 6.844c.85.004 1.705.115 2.504.337 1.909-1.296 2.747-1.027 2.747-1.027.546 1.379.202 2.398.1 2.651.64.7 1.028 1.595 1.028 2.688 0 3.848-2.339 4.695-4.566 4.943.359.309.678.92.678 1.855 0 1.338-.012 2.419-.012 2.747 0 .268.18.58.688.482A10.019 10.019 0 0022 12.017C22 6.484 17.522 2 12 2z"
+              clip-rule="evenodd"
+            />
+          </svg>
+        </a>
+      </li>
 
-    <li>
-      <a
-        href="https://www.linkedin.com/in/robin-augereau/"
-        rel="noreferrer"
-        target="_blank"
-        class="text-gray-700 transition hover:text-gray-700/75"
-      >
-        <span class="sr-only">LinkedIn</span>
-        <svg
-        xmlns="http://www.w3.org/2000/svg"
-        class="xl:h-6 xl:w-6 sm:w-20 sm:h-20 sm:-mt-2"
-        fill="currentColor"
-        viewBox="0 0 24 24">
-        <path
-          d="M4.98 3.5c0 1.381-1.11 2.5-2.48 2.5s-2.48-1.119-2.48-2.5c0-1.38 1.11-2.5 2.48-2.5s2.48 1.12 2.48 2.5zm.02 4.5h-5v16h5v-16zm7.982 0h-4.968v16h4.969v-8.399c0-4.67 6.029-5.052 6.029 0v8.399h4.988v-10.131c0-7.88-8.922-7.593-11.018-3.714v-2.155z" />
-      </svg>
-      </a>
-    </li>
-  </ul>
+      <li>
+        <a
+          href="https://www.instagram.com/newton.creations/"
+          rel="noreferrer"
+          target="_blank"
+          class="text-gray-700 transition hover:text-gray-700/75"
+        >
+          <span class="sr-only">Instagram</span>
+          <svg
+            class="lg:h-7 lg:w-7 sm:w-20 sm:h-20"
+            fill="currentColor"
+            viewBox="0 0 24 24"
+            aria-hidden="true"
+          >
+            <path
+              fill-rule="evenodd"
+              d="M12.315 2c2.43 0 2.784.013 3.808.06 1.064.049 1.791.218 2.427.465a4.902 4.902 0 011.772 1.153 4.902 4.902 0 011.153 1.772c.247.636.416 1.363.465 2.427.048 1.067.06 1.407.06 4.123v.08c0 2.643-.012 2.987-.06 4.043-.049 1.064-.218 1.791-.465 2.427a4.902 4.902 0 01-1.153 1.772 4.902 4.902 0 01-1.772 1.153c-.636.247-1.363.416-2.427.465-1.067.048-1.407.06-4.123.06h-.08c-2.643 0-2.987-.012-4.043-.06-1.064-.049-1.791-.218-2.427-.465a4.902 4.902 0 01-1.772-1.153 4.902 4.902 0 01-1.153-1.772c-.247-.636-.416-1.363-.465-2.427-.047-1.024-.06-1.379-.06-3.808v-.63c0-2.43.013-2.784.06-3.808.049-1.064.218-1.791.465-2.427a4.902 4.902 0 011.153-1.772A4.902 4.902 0 015.45 2.525c.636-.247 1.363-.416 2.427-.465C8.901 2.013 9.256 2 11.685 2h.63zm-.081 1.802h-.468c-2.456 0-2.784.011-3.807.058-.975.045-1.504.207-1.857.344-.467.182-.8.398-1.15.748-.35.35-.566.683-.748 1.15-.137.353-.3.882-.344 1.857-.047 1.023-.058 1.351-.058 3.807v.468c0 2.456.011 2.784.058 3.807.045.975.207 1.504.344 1.857.182.466.399.8.748 1.15.35.35.683.566 1.15.748.353.137.882.3 1.857.344 1.054.048 1.37.058 4.041.058h.08c2.597 0 2.917-.01 3.96-.058.976-.045 1.505-.207 1.858-.344.466-.182.8-.398 1.15-.748.35-.35.566-.683.748-1.15.137-.353.3-.882.344-1.857.048-1.055.058-1.37.058-4.041v-.08c0-2.597-.01-2.917-.058-3.96-.045-.976-.207-1.505-.344-1.858a3.097 3.097 0 00-.748-1.15 3.098 3.098 0 00-1.15-.748c-.353-.137-.882-.3-1.857-.344-1.023-.047-1.351-.058-3.807-.058zM12 6.865a5.135 5.135 0 110 10.27 5.135 5.135 0 010-10.27zm0 1.802a3.333 3.333 0 100 6.666 3.333 3.333 0 000-6.666zm5.338-3.205a1.2 1.2 0 110 2.4 1.2 1.2 0 010-2.4z"
+              clip-rule="evenodd"
+            />
+          </svg>
+        </a>
+      </li>
+    </ul>
+  </div>
 </div>
