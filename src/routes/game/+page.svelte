76c09a6d3c6de0c2834f8be1e36edad27df8d9f8
newtondotcom <asphalt9fr@gmail.com>
2023-12-18 17:14:27 +0100

    bind hand

--- src/routes/game/+page.svelte
+++ src/routes/game/+page.svelte
@@ -1,10 +1,10 @@
 <script lang="ts">
-    import Deck from "$lib/cards/deck.svelte";
-    import Fake from "$lib/cards/fake.svelte";
-    import River from "$lib/cards/river.svelte";
+    import Deck from "$lib/cards/Deck.svelte";
+    import Fake from "$lib/cards/Fake.svelte";
+    import River from "$lib/cards/River.svelte";
     import Filter from "$lib/ui/Filter.svelte";
     import Turn from "$lib/ui/Turn.svelte";
-    import { generateRandomDeck, generateRiver } from "$lib/utils/deck";
+    import { generateRandomDeck, generateRiver} from "$lib/utils/deck";
 
     let hand = generateRandomDeck();
     let river = generateRiver();
@@ -13,9 +13,8 @@
 <div id="main">
   <Deck {hand} />
   <Turn side="down" />
-  <Fake side="top" qqty={8} />
-  <Fake side="left" qqty={8} />
-  <Fake side="right" qqty={8} />
+  <Fake side="left" qqty={4} />
+  <Fake side="right" qqty={4} />
   <River {river} />
-  <Filter ordreAtout={true} ordreValeur={true} />
+  <Filter bind:hand/>
 </div>
