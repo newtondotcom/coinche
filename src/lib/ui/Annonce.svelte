45b09af1a91cd6898131e964aff93e542328d1a9
newtondotcom <asphalt9fr@gmail.com>
2024-03-10 20:45:26 +0100

    added button to coinche and pass

--- src/lib/ui/Annonce.svelte
+++ src/lib/ui/Annonce.svelte
@@ -1,25 +1,34 @@
 <script lang="ts">
 import Modal from "$lib/ui/Modal.svelte";
-let open = true;
+export let annonceOuverte = false;
 let toogle = () => {
-    open = !open;
+    annonceOuverte = !annonceOuverte;
 };
 let loading = false;
 let annonces = [80,90,100,110,120,130,140,150]
 let suites = ['♠️','♥️','♦️','♣️']
+
+let joinParty = () => {
+    loading = true;
+    setTimeout(() => {
+        loading = false;
+        toogle();
+    }, 2000);
+};
+
 </script>
 
 <Modal
   title="Annonce moi ca comme un roi 👋"
-  open={open}
+  open={annonceOuverte}
   on:close={toogle}
 >
   <svelte:fragment slot="body">
     <div class="flex flex-row align-middle justify-center">
-        <div class="flex grid grid-cols-4 gap-4">
+        <div class="grid grid-cols-4 gap-4">
                 {#each annonces as annonce}
                     <div
-                    class="w-full rounded-md border-gray-400 pe-10 px-5 py-2.5 shadow-sm sm:text-sm place-content-center text-black"
+                    class="w-full h-full rounded-md border-gray-500 px-5 py-2.5 shadow-sm sm:text-sm place-content-center items-center text-black"
                     >
                     {annonce}
                     </div>
@@ -28,7 +37,7 @@ let suites = ['♠️','♥️','♦️','♣️']
         <div class="grid-cols-4 gap-4">
             {#each suites as annonce}
                 <div
-                class="w-full rounded-md border-gray-400 pe-10 px-5 py-2.5 shadow-sm sm:text-sm place-content-center text-black"
+                class="w-full rounded-md border-gray-500 px-5 py-2.5 shadow-sm sm:text-sm place-content-center items-center text-black"
                 >
                 {annonce}
                 </div>
@@ -36,17 +45,19 @@ let suites = ['♠️','♥️','♦️','♣️']
     </div>
 
 
-    <div>
+    <div class="flex flex-col">
+        <div >Annonce actuelle : 100 coeur</div>
         <button
-        class="ml-8 rounded-md bg-teal-600 px-5 py-2.5 text-sm font-medium text-white shadow text-center"
+        class="flex rounded-md bg-teal-600 px-5 py-2.5 text-sm font-medium text-white shadow text-center"
         on:click={() => joinParty()}
         >
-        {#if !loading}
-        Rejoindre
-        {:else}
-        <svg class="animate-spin h-5 w-5 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"><circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="1.5"></circle><path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3.169 1.952 1.952 4.538 3.13 7.35 3.13V17z"></path></svg>
-        {/if}
-
+        Passer
+        </button>
+        <button
+        class="flex rounded-md bg-teal-600 px-5 py-2.5 text-sm font-medium text-white shadow text-center"
+        on:click={() => joinParty()}
+        >
+        Coincher
         </button>
     </div>
   </div>
