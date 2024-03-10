5c71130f8f52d760665a6a72f917b63e31b1a990
newtondotcom <asphalt9fr@gmail.com>
2024-03-10 20:46:00 +0100

    modal when player is coinched

---
+++ src/lib/ui/Coinched.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+import Modal from "$lib/ui/Modal.svelte";
+export let getCoinched = false;
+let toogle = () => {
+    getCoinched = !getCoinched;
+};
+
+</script>
+
+<Modal
+  title="T'as été coinché !? ⚠️"
+  open={getCoinched}
+  on:close={toogle}
+>
+  <svelte:fragment slot="body">
+    <div class="flex flex-row justify-center">
+        <button
+        class="flex rounded-md bg-teal-600 px-5 py-2.5 text-sm font-medium text-white shadow text-center mr-6"
+        on:click={() => getCoinched = false}
+        >
+        Passer
+        </button>
+        <button
+        class="flex rounded-md bg-teal-600 px-5 py-2.5 text-sm font-medium text-white shadow text-center"
+        on:click={() => getCoinched = false}
+        >
+        Surcoincher
+        </button>
+    </div>
+  </svelte:fragment>
+</Modal>
