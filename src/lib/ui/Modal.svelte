cd81f01ad7fb947667b9164c7c45050faac5ddf0
newtondotcom <asphalt9fr@gmail.com>
2024-03-10 20:48:59 +0100

    try to reduce unwanted overflow

--- src/lib/ui/Modal.svelte
+++ src/lib/ui/Modal.svelte
@@ -1,7 +1,7 @@
 {#if open}
-<div class="modal z-50 fixed w-full h-full top-0 left-0 flex items-center justify-center p-8 lg:p-0">
+<div class="modal z-50 fixed w-full h-full top-0 left-0 flex items-center justify-center">
   <div class="modal-overlay fixed w-full h-full bg-gray-900 opacity-50"></div>
-  <div class="bg-white w-full lg:h-max lg:w-1/2  mx-auto rounded-lg shadow-xl z-50 overflow-y-auto">
+  <div class="bg-white sm:w-4/5 lg:h-max lg:w-1/2  mx-auto rounded-lg shadow-xl z-50">
     <div class="flex justify-between items-center head bg-gray-100 py-5 px-8 text-2xl font-extrabold">
       {title}
       <button class="p-2 bg-gray-200 hover:bg-gray-300 rounded-full ml-4" on:click={() => dispatch('close')}>
