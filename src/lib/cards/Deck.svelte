6772034a48b310505c2b37e3d5403534f8130dd7
newtondotcom <asphalt9fr@gmail.com>
2024-03-13 10:33:07 +0100

    bring back wrong called files

---
+++ src/lib/cards/Deck.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+    import Card from "$lib/cards/Card.svelte";
+    export let hand : IPlayCard[];
+</script>
+
+<div class="flex flex-row absolute -bottom-14 justify-center w-full">
+{#each hand as card}
+    <Card suite={card.suite} value={card.value} />
+{/each}
+</div>
---
+++ src/lib/cards/Fake.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+	import { maxCardWidth } from '$lib/constants';
+  
+	export let side: "left" | "right" | "top";
+	export let qqty: number;
+  
+	let tabGen = (qty: number) => {
+	  let tab = [];
+	  for (let i = 0; i < qty; i++) {
+		tab.push(i);
+	  }
+	  return tab;
+	}
+  </script>
+  
+  <div
+	class="{side === 'left' ? 'flex flex-col absolute -left-20 h-full justify-center items-center' : side === 'right' ? 'flex flex-col absolute -right-20 h-full justify-center items-center' : 'flex flex-row absolute top-20 justify-center w-full shrink'}"
+  >
+	{#each tabGen(qqty) as i}
+	  <img
+		src="/dos.webp"
+		alt="dos"
+		style="max-width:{maxCardWidth + 20}px; height: auto; transform: translateY({side === 'left' ? -i * 20 : side === 'right' ? -i * 20 : side === 'top' ? -20 : 0}px) translateX({side === 'top' ? -i * 20 : 0}px);z-index:1;"
+		class="{side === 'top' ? '-rotate-90' : ''} pointer-events-none"
+	  />
+	{/each}
+  </div>
+  
