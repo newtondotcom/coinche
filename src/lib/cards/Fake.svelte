7652d86b85a3950fa681431268001cb86e6ec6d0
newtondotcom <asphalt9fr@gmail.com>
2024-10-23 11:47:14 +0200

    fix : move to displaying a card packet with the number of remaining cards

--- src/lib/cards/Fake.svelte
+++ src/lib/cards/Fake.svelte
@@ -1,28 +1,20 @@
 <script lang="ts">
 	import { maxCardWidth } from '$lib/constants';
+	import { Badge } from "$lib/components/ui/badge/index.js";
   
 	export let side: "left" | "right" | "top";
 	export let qqty: number;
-  
-	let tabGen = (qty: number) => {
-	  let tab = [];
-	  for (let i = 0; i < qty; i++) {
-		tab.push(i);
-	  }
-	  return tab;
-	}
   </script>
   
   <div
-	class="{side === 'left' ? 'flex flex-col absolute -left-20 h-full justify-center items-center' : side === 'right' ? 'flex flex-col absolute -right-20 h-full justify-center items-center' : 'flex flex-row absolute top-20 justify-center w-full shrink'}"
+	class="{side === 'left' ? 'flex flex-row absolute -left-0 h-full justify-center items-center' : side === 'right' ? 'flex flex-row absolute -right-0 h-full justify-center items-center' : 'flex flex-col absolute top-0 justify-center w-full shrink'}"
   >
-	{#each tabGen(qqty) as i}
-	  <img
-		src="/dos.webp"
-		alt="dos"
-		style="max-width:{maxCardWidth + 20}px; height: auto; transform: translateY({side === 'left' ? -i * 20 : side === 'right' ? -i * 20 : side === 'top' ? -20 : 0}px) translateX({side === 'top' ? -i * 20 : 0}px);z-index:1;"
-		class="{side === 'top' ? '-rotate-90' : ''} pointer-events-none"
-	  />
-	{/each}
+  <img
+	src="/dos.webp"
+	alt="dos"
+	style="max-width:{maxCardWidth + 20}px; height: auto; transform: translateY({side === 'left' ? -side * 20 : side === 'right' ? -side * 20 : side === 'top' ? -0 : 0}px) translateX({side === 'top' ? -side * 20 : 0}px);z-index:1;"
+	class="{side === 'top' ? '-rotate-90 self-center' : ''} pointer-events-none"
+  />
+  <Badge class="mx-auto">{qqty}</Badge>
   </div>
   
