10e7ee64468e33f07b6240c2ea7f3d6359f1a2fc
newtondotcom <asphalt9fr@gmail.com>
2024-10-26 16:03:02 +0200

    feat : displaying point of current game and total points

---
+++ components/coinche/interface/points.vue
@@ -0,0 +1,61 @@
+<template>
+    <Card class="w-[250px] mx-2 my-2">
+        <CardHeader>
+            <CardTitle>Points de la partie</CardTitle>
+        </CardHeader>
+        <CardContent class="grid gap-6">
+            <div class="flex items-center justify-between space-x-4">
+                <div class="flex items-center space-x-4">
+                    <Avatar>
+                        <AvatarFallback>82</AvatarFallback>
+                    </Avatar>
+                    <div>
+                        <p class="text-sm font-medium leading-none">Benzeze X Tidani</p>
+                        <p class="text-sm text-muted-foreground">23 ème / 3 ème</p>
+                    </div>
+                </div>
+            </div>
+            <div class="flex items-center justify-between space-x-4">
+                <div class="flex items-center space-x-4">
+                    <Avatar>
+                        <AvatarFallback>23</AvatarFallback>
+                    </Avatar>
+                    <div>
+                        <p class="text-sm font-medium leading-none">Fefe X Robebs</p>
+                        <p class="text-sm text-muted-foreground">19 ème / 82 ème</p>
+                    </div>
+                </div>
+            </div>
+        </CardContent>
+    </Card>
+
+    <Card class="w-[250px] mx-2 my-2">
+        <CardHeader>
+            <CardTitle>Points globaux</CardTitle>
+        </CardHeader>
+        <CardContent class="grid gap-6">
+            <div class="flex items-center justify-between space-x-4">
+                <div class="flex items-center space-x-4">
+                    <Avatar>
+                        <AvatarFallback>170</AvatarFallback>
+                    </Avatar>
+                    <div>
+                        <p class="text-sm font-medium leading-none">Benzeze X Tidani</p>
+                        <p class="text-sm text-muted-foreground">23 ème / 3 ème</p>
+                    </div>
+                </div>
+            </div>
+            <div class="flex items-center justify-between space-x-4">
+                <div class="flex items-center space-x-4">
+                    <Avatar>
+                        <AvatarFallback>722</AvatarFallback>
+                    </Avatar>
+                    <div>
+                        <p class="text-sm font-medium leading-none">Fefe X Robebs</p>
+                        <p class="text-sm text-muted-foreground">19 ème / 82 ème</p>
+                    </div>
+                </div>
+            </div>
+        </CardContent>
+    </Card>
+</template>
