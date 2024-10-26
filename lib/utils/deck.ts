6e964f7fcff84ac3078f2a16fb0fd6f978ce2b73
newtondotcom <asphalt9fr@gmail.com>
2024-10-26 17:18:13 +0200

    feat : huge function to assign value to cards depending of atout

--- lib/utils/deck.ts
+++ lib/utils/deck.ts
@@ -1,8 +1,9 @@
 const values: CardValue[] = ['7', '8', '9', 'J', 'Q', 'K', '10', 'A'];
+const valuesNum: number[] = [0, 0, 0, 2, 3, 4, 10, 11];
 const suites: CardSuite[] = ['diamonds', 'clubs', 'hearts', 'spades'];
 
-export function generateRandomDeck(): IPlayCard[] {
-    const deck: IPlayCard[] = [];
+export function generateRandomDeck(): ICard[] {
+    const deck: ICard[] = [];
 
     while (deck.length < 8) {
         const randomSuite: CardSuite = suites[Math.floor(Math.random() * suites.length)];
@@ -19,26 +20,126 @@ export function generateRandomDeck(): IPlayCard[] {
     return deck;
 }
 
-export function generateRiver(): IPlayCard[] {
-    const deck: IPlayCard[] = [];
-
-    while (deck.length < 4) {
-        const randomSuite: CardSuite = suites[Math.floor(Math.random() * suites.length)];
-        const randomValue: CardValue = values[Math.floor(Math.random() * values.length)];
-        const key: string = Math.random().toString();
-        const valueNum: number = values.indexOf(randomValue) + 1;
-        const isDuplicate = deck.some(
-            (card) => card.suite === randomSuite && card.value === randomValue,
-        );
-        if (!isDuplicate) {
-            deck.push({ suite: randomSuite, value: randomValue, key, valueNum });
+export function setValueAccordingToAtout(atout: CardSuite, deck: ICard[]): ICard[] {
+    return deck.map((card) => {
+        // Traditional Atout: Set values for the chosen atout suit
+        if (atout !== 'tout-atout' && atout !== 'sans-atout') {
+            if (card.suite === atout) {
+                switch (card.value) {
+                    case '7':
+                    case '8':
+                    case '9':
+                        card.valueNum = 0;
+                        break;
+                    case 'J':
+                        card.valueNum = 2;
+                        break;
+                    case 'Q':
+                        card.valueNum = 3;
+                        break;
+                    case 'K':
+                        card.valueNum = 4;
+                        break;
+                    case '10':
+                        card.valueNum = 10;
+                        break;
+                    case 'A':
+                        card.valueNum = 11;
+                        break;
+                    default:
+                        break;
+                }
+            } else {
+                // Set default values for non-atout cards
+                switch (card.value) {
+                    case '7':
+                    case '8':
+                    case '9':
+                        card.valueNum = 0;
+                        break;
+                    case 'J':
+                        card.valueNum = 2;
+                        break;
+                    case 'Q':
+                        card.valueNum = 3;
+                        break;
+                    case 'K':
+                        card.valueNum = 4;
+                        break;
+                    case '10':
+                        card.valueNum = 10;
+                        break;
+                    case 'A':
+                        card.valueNum = 11;
+                        break;
+                    default:
+                        break;
+                }
+            }
         }
-    }
-    return deck;
+        // Sans-Atout: Set values for all cards (no atout suit)
+        else if (atout === 'sans-atout') {
+            switch (card.value) {
+                case 'J':
+                    card.valueNum = 2;
+                    break;
+                case 'Q':
+                    card.valueNum = 3;
+                    break;
+                case 'K':
+                    card.valueNum = 4;
+                    break;
+                case '10':
+                    card.valueNum = 10;
+                    break;
+                case 'A':
+                    card.valueNum = 19;
+                    break;
+                case '7':
+                case '8':
+                case '9':
+                    card.valueNum = 0;
+                    break;
+                default:
+                    break;
+            }
+        }
+        // Tout-Atout: All cards are assigned specific values
+        else if (atout === 'tout-atout') {
+            switch (card.value) {
+                case '9':
+                    card.valueNum = 9;
+                    break;
+                case 'J':
+                    card.valueNum = 14;
+                    break;
+                case 'Q':
+                    card.valueNum = 2;
+                    break;
+                case 'K':
+                    card.valueNum = 3;
+                    break;
+                case '10':
+                    card.valueNum = 5;
+                    break;
+                case 'A':
+                    card.valueNum = 7;
+                    break;
+                case '7':
+                case '8':
+                    card.valueNum = 0;
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        return card;
+    });
 }
 
-export function orderCards(cards: IPlayCard[], value: boolean, tri: boolean): IPlayCard[] {
-    let orderedCards: IPlayCard[] = [...cards];
+export function orderCards(cards: ICard[], value: boolean, tri: boolean): ICard[] {
+    let orderedCards: ICard[] = [...cards];
 
     let diamonds = orderedCards.filter((card) => card.suite === 'diamonds');
     let clubs = orderedCards.filter((card) => card.suite === 'clubs');
