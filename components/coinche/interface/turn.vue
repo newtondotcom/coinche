e3732c980fdddd84c1f693eb66f619fc64447e0d
newtondotcom <asphalt9fr@gmail.com>
2024-10-26 16:03:13 +0200

    feat : display of card remaning and turn

---
+++ components/coinche/interface/turn.vue
@@ -0,0 +1,33 @@
+<template>
+    <Card class="w-[350px] h-[200px] mx-2">
+        <CardHeader></CardHeader>
+        <CardContent>
+            <div class="grid grid-cols-3 gap-4 w-full max-w-md mx-auto">
+                <div></div>
+                <div class="flex justify-center items-center">
+                    <Badge>1</Badge>
+                    <span class="font-semibold mx-1">Benzeze</span>
+                </div>
+                <div></div>
+
+                <div class="flex justify-center items-center">
+                    <Badge variant="secondary">1</Badge>
+                    <span class="font-semibold mx-1">Fefe</span>
+                </div>
+                <div></div>
+                <div class="flex justify-center items-center">
+                    <Badge variant="secondary">1</Badge>
+                    <span class="font-semibold mx-1">Tidani</span>
+                </div>
+
+                <div></div>
+                <div class="flex justify-center items-center">
+                    <Badge variant="secondary">1</Badge>
+                    <span class="font-semibold mx-1">Robebs</span>
+                </div>
+                <div></div>
+            </div>
+        </CardContent>
+        <CardFooter></CardFooter>
+    </Card>
+</template>
