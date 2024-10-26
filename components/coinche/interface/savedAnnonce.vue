cd1685dccad260cb1d77bd66fb7744db9e59bd42
newtondotcom <asphalt9fr@gmail.com>
2024-10-26 16:02:46 +0200

    feat : display saved annonce

---
+++ components/coinche/interface/savedAnnonce.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="absolute left-[50%] top-[30%] -translate-x-[50%]">
+        <h2
+            class="bg-opacity-50 bg-gradient-to-b from-neutral-200 to-neutral-400 bg-clip-text text-center text-3xl font-bold text-transparent dark:from-neutral-50 dark:to-neutral-400 md:text-7xl cursor-pointer"
+        >
+            100 ♥
+        </h2>
+        <h3
+            class="bg-opacity-50 bg-gradient-to-b from-neutral-200 to-neutral-400 bg-clip-text text-center text-3xl font-bold text-transparent dark:from-neutral-50 dark:to-neutral-400 md:text-5xl cursor-pointer"
+        >
+            par Benz
+        </h3>
+    </div>
+</template>
