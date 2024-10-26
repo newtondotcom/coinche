9810fed19cb7e68330daf8d85033c238d2347be0
newtondotcom <asphalt9fr@gmail.com>
2024-10-26 14:26:49 +0200

    feat : start of a layout

--- app.vue
+++ app.vue
@@ -1,5 +1,14 @@
 <template>
-  <div>
+  <div class="bg-secondary min-h-screen">
+    <header class="px-2 py-2 flex flex-row justify-between">
+      <h1
+			class="bg-opacity-50 bg-gradient-to-b from-neutral-400 to-primary bg-clip-text text-center text-3xl font-bold text-transparent dark:from-neutral-50 dark:to-neutral-400 md:text-4xl cursor-pointer"
+		>
+    Coinche.xyz
+		</h1>
+      <CoincheColorMode />
+    </header>
     <NuxtPage />
+    <footer></footer>
   </div>
 </template>
