f8cb99b16e8a8d6636b0c0e26c95531c05e0a7ed
newtondotcom <asphalt9fr@gmail.com>
2024-10-28 00:22:34 +0100

    fix : background annonce on screen

--- components/coinche/interface/savedAnnonce.vue
+++ components/coinche/interface/savedAnnonce.vue
@@ -3,12 +3,28 @@
         <h2
             class="bg-opacity-50 bg-gradient-to-b from-neutral-200 to-neutral-400 bg-clip-text text-center text-3xl font-bold text-transparent dark:from-neutral-50 dark:to-neutral-400 md:text-7xl cursor-pointer"
         >
-            100 ♥
+            {{ storeGame.game.last_annonce.annonce }}
+
+            {{ storeGame.game.last_annonce.suite == 'clubs' ? '♣' : '' }}
+            {{ storeGame.game.last_annonce.suite == 'diamonds' ? '♦' : '' }}
+            {{ storeGame.game.last_annonce.suite == 'hearts' ? '♥' : '' }}
+            {{ storeGame.game.last_annonce.suite == 'spades' ? '♠' : '' }}
+            {{ storeGame.game.last_annonce.suite == 'sans-atout' ? 'SA' : '' }}
+            {{ storeGame.game.last_annonce.suite == 'tout-atout' ? 'TA' : '' }}
         </h2>
         <h3
             class="bg-opacity-50 bg-gradient-to-b from-neutral-200 to-neutral-400 bg-clip-text text-center text-3xl font-bold text-transparent dark:from-neutral-50 dark:to-neutral-400 md:text-5xl cursor-pointer"
         >
-            par Benz
+            par
+            {{
+                storePlayers.players.find((p) => p.id === storeGame.game.last_annonce.playerId)
+                    ?.surname
+            }}
         </h3>
     </div>
 </template>
+
+<script setup lang="ts">
+    const storeGame = useGameStore();
+    const storePlayers = usePlayersStore();
+</script>
