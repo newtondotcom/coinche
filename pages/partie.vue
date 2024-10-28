176575bfb9a1c246c0d66a8d5e73d8db94153475
newtondotcom <asphalt9fr@gmail.com>
2024-10-28 22:14:35 +0100

    fix : directly trigger listener

--- pages/partie.vue
+++ pages/partie.vue
@@ -3,7 +3,7 @@
 
     <CoincheRiver />
 
-    <CoincheInterfaceAnnonces v-if="timeToAnnonce" :emitAnnonce="emitAnnonce" />
+    <CoincheInterfaceAnnonces v-if="storeAbout.timeToAnnonce" />
 
     <div class="flex flex-row justify-between" v-if="storePlayers.players.length == 4">
         <CoincheInterfacePoints />
@@ -18,6 +18,7 @@
 <script setup lang="ts">
     import { useToast } from '@/components/ui/toast/use-toast';
     import { join, leave } from '@/lib/supabase/io';
+    import translateEvent from '@/lib/utils/listener';
     import { createClient } from '@supabase/supabase-js';
 
     const { toast } = useToast();
@@ -38,8 +39,6 @@
 
     let turn = ref<boolean>(true);
     watch(turn, () => {});
-
-    let timeToAnnonce = ref<boolean>(false);
 
     async function cardPressed(suite: CardSuite, value: CardValue) {
         const selectedCardIndex = storeAbout.hand.findIndex(
@@ -62,13 +61,8 @@
         console.log('highestAtoutInPli : ', storeAbout.highestAtoutInPli);
     }
 
-    async function emitAnnonce(ann: IAnnonce) {
-        storeGame.setLastAnnonce(ann);
-        console.log('Annonce faite : ', storeGame.game.last_annonce);
-    }
-
     onMounted(async () => {
-        startListening();
+        await startListening();
         join();
     });
 
@@ -81,7 +75,7 @@
         const supabase = createClient(config.public.SUPABASE_URL, config.public.SUPABASE_ANON_KEY);
 
         const handleInserts = (payload: any) => {
-            console.log('Change received!', payload.new);
+            translateEvent(payload.new);
         };
 
         supabase
