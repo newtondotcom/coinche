0435e7537e9ec43ceb91a3c7c1f7a11ed08684de
newtondotcom <asphalt9fr@gmail.com>
2024-10-28 22:14:50 +0100

    fix : changes to score on game store

--- stores/players.ts
+++ stores/players.ts
@@ -8,11 +8,8 @@ export const usePlayersStore = defineStore('players', () => {
         }
     }
 
-    function addScore(score: number, playerId: string) {
-        const player = players.value.find((player) => player.id === playerId);
-        if (player) {
-            player.score += score;
-        }
+    function addPlayer(player: IPlayer) {
+        players.value.push(player);
     }
 
     function setPosition(position: PlayerPosition, playerId: string) {
@@ -29,5 +26,5 @@ export const usePlayersStore = defineStore('players', () => {
         players.value.filter((p) => p.position === 1 || p.position === 3),
     );
 
-    return { players, team1, team2, setHands, addScore, setPosition };
+    return { players, team1, team2, setHands, setPosition, addPlayer };
 });
