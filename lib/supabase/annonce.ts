d6c67225c59d809fb3647f796a15b195366bb2ed
newtondotcom <asphalt9fr@gmail.com>
2024-10-29 13:18:27 +0100

    fix : trigger game start if 3 passes are counted

--- lib/supabase/annonce.ts
+++ lib/supabase/annonce.ts
@@ -6,6 +6,7 @@ const config = useRuntimeConfig();
 const supabase = createClient(config.public.SUPABASE_URL, config.public.SUPABASE_ANON_KEY);
 
 export default async function emitAnnonce(annonce: IAnnonce) {
+    const storeGame = useGameStore();
     const storeAbout = useAboutStore();
     const gameId = storeAbout.gameId;
     await supabase.from('Events').insert([
@@ -17,6 +18,31 @@ export default async function emitAnnonce(annonce: IAnnonce) {
             value: formatAnnonce(annonce),
         },
     ]);
+
+    // if pass
+    if (annonce.annonce === 0) {
+        // check if this is not the last pass possible
+        const annoncesPassed = storeGame.annonces_pli.filter((annonce) => annonce.annonce === 0);
+        if (annoncesPassed.length === 2) {
+            // only 2 because the current annonce is passed
+            storeGame.setNewPli();
+            storeGame.setCurrentPlayerId(storeGame.player_starting_id);
+            // launch game
+            await supabase.from('Events').insert([
+                {
+                    id: await genIdCuid(),
+                    type: 'start_pli',
+                    playerId: storeAbout.myId,
+                    gameId: storeAbout.gameId,
+                    value: storeGame.player_starting_id, // value is the name of the player starting the pli
+                },
+            ]);
+            console.log('Starting pli because of 3 passes');
+            return 'Pli terminé';
+        } else {
+            console.log(annoncesPassed.length, 'passes');
+        }
+    }
 }
 
 export function formatAnnonce(annonce: IAnnonce): string {
@@ -30,4 +56,14 @@ export function deformatAnnonce(annonce: string, playerId: string): IAnnonce {
         suite: suite as CardSuite,
         playerId: playerId,
     };
+}
+
+export function setNextPlayerTurn(playerId: string) {
+    const storePlayers = usePlayersStore();
+    const currentPlayerIndex = storePlayers.players.findIndex(
+        (player: IPlayer) => player.id === playerId,
+    );
+    const nextPlayerIndex = (currentPlayerIndex + 1) % storePlayers.players.length;
+    const nextPlayerId = storePlayers.players[nextPlayerIndex].id;
+    return nextPlayerId;
 }
