fe177ff0829fa3fa4c82ecde41a080d7c596c211
newtondotcom <asphalt9fr@gmail.com>
2024-10-29 13:07:09 +0100

    fix : add coinched and surcoinched badge

--- components/coinche/interface/savedAnnonce.vue
+++ components/coinche/interface/savedAnnonce.vue
@@ -20,6 +20,12 @@
                 storePlayers.players.find((p) => p.id === storeGame.last_annonce.playerId)?.surname
             }}
         </h3>
+        <h4
+            class="bg-opacity-50 bg-gradient-to-b from-neutral-200 to-neutral-400 bg-clip-text text-center text-3xl font-bold text-transparent dark:from-neutral-50 dark:to-neutral-400 md:text-4xl cursor-pointer"
+        >
+            <Badge v-if="storeGame.coinched">Coinché</Badge>
+            <Badge v-if="storeGame.surcoinched">Coinché</Badge>
+        </h4>
     </div>
 </template>
 
