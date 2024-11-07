b3342e0f8dc52f98a9af084a1196ea2feca9639c
newtondotcom <asphalt9fr@gmail.com>
2024-11-07 17:11:43 +0100

    feat : component to display while distributiibg cards

---
+++ components/coinche/interface/distribution.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="fixed inset-0 z-50 flex items-center justify-center bg-gray-900 bg-opacity-50">
+        <Card>
+            <CardHeader>
+                <CardTitle>Distribution</CardTitle>
+                <CardDescription>en cours</CardDescription>
+            </CardHeader>
+            <CardContent>
+                <div
+                    class="animate-spin rounded-full h-32 w-32 border-t-2 border-b-2 border-neutral-200"
+                ></div>
+            </CardContent>
+        </Card>
+    </div>
+</template>
+
+<script setup lang="ts"></script>
