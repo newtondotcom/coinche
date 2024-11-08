b7f47efa2a3c3cd0b83aae60e565dade7092df03
newtondotcom <asphalt9fr@gmail.com>
2024-11-08 11:59:36 +0100

    fix : client function to emit pli winning

--- lib/supabase/pli.ts
+++ lib/supabase/pli.ts
@@ -1,5 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
+import { deformatCarteToDistribute } from './distribution';
 import genIdCuid from './gen';
 
 const config = useRuntimeConfig();
@@ -8,21 +9,26 @@ const supabase = createClient(config.public.SUPABASE_URL, config.public.SUPABASE
 export async function closePli() {
     const storeAbout = useAboutStore();
     const storeGame = useGameStore();
-    // fetch all the pli events
-    const { data: existingEvents, error: selectError } = await supabase
-        .from('Events')
-        .select('*')
-        .eq('gameId', storeAbout.gameId)
-        .eq('type', 'play');
-    if (selectError) {
-        console.error('Error fetching events', selectError);
-        return;
-    }
-    // fetch the last pli event
-    const lastPliId = storeGame.pli_number;
-    const lastPliEvents = existingEvents.filter((event) => event.value.pli_number === lastPliId);
+    const storePlayers = usePlayersStore();
 
-    return 'pli';
+    // find the winner
+    const pastPlis: IPlay[] = await fetchLastPliEvents();
+    const winnerPlayerId = findWinner(pastPlis);
+    const myIndex = storePlayers.players.findIndex(
+        (player: IPlayer) => player.id === winnerPlayerId,
+    );
+    const partner = storePlayers.players[(myIndex + 2) % 4];
+    const teamMatePlayerId = partner.id;
+    await supabase.from('Events').insert([
+        {
+            id: await genIdCuid(),
+            type: 'win_pli',
+            playerId: storeAbout.myId,
+            gameId: storeAbout.gameId,
+            value: formatTeam(storeAbout.myId, teamMatePlayerId),
+        },
+    ]);
+    return;
 }
 
 export async function startPli() {
@@ -42,6 +48,66 @@ export async function startPli() {
     ]);
 }
 
-export function formatPoseCarte(card: ICard, pli_number: number): string {
-    return `${card.value}|${card.suite}`;
+function findWinner(lastPliEvents: IPlay[]) {
+    const storeAbout = useAboutStore();
+    const atout = storeAbout.atout;
+    if (lastPliEvents.some((pli) => pli.card.suite === atout)) {
+        // atout is played
+        const atoutCards = lastPliEvents.filter((pli) => pli.card.suite === atout);
+        const highestAtout = atoutCards.reduce((acc, card) => {
+            if (card.card.valueNum > acc.card.valueNum) {
+                return card;
+            }
+            return acc;
+        });
+        return highestAtout.playerId;
+    } else {
+        // no atout played
+        const firstSuite = lastPliEvents[0].card.suite;
+        const sameSuite = lastPliEvents.filter((pli) => pli.card.suite === firstSuite);
+        const highestSameSuite = sameSuite.reduce((acc, card) => {
+            if (card.card.valueNum > acc.card.valueNum) {
+                return card;
+            }
+            return acc;
+        });
+        return highestSameSuite.playerId;
+    }
+}
+
+export function formatTeam(player1: string, player2: string): string {
+    return [player1, player2].sort().join('-');
+}
+
+export function deformatTeam(team: string): string[] {
+    return team.split('-');
+}
+
+export async function fetchLastPliEvents(): Promise<IPlay[]> {
+    const storeAbout = useAboutStore();
+    const storeGame = useGameStore();
+    // fetch all the pli events
+    const { data: existingEvents, error: selectError } = await supabase
+        .from('Events')
+        .select('*')
+        .eq('gameId', storeAbout.gameId)
+        .eq('type', 'play');
+    if (selectError) {
+        console.error('Error fetching events', selectError);
+        return [];
+    }
+    // fetch the last pli event
+    const lastPliId = storeGame.pli_number;
+    const lastPliEvents = existingEvents.filter(
+        (event) => deformatCarteToDistribute(event.value).pli_number === lastPliId,
+    );
+    const pastPlis: IPlay[] = lastPliEvents.map((event) => ({
+        card: deformatCarteToDistribute(event.value).card,
+        playerId: event.playerId,
+    }));
+    return pastPlis;
+}
+
+export function sumPointsPli(plays: IPlay[]) {
+    return plays.reduce((acc, play) => acc + play.card.valueNum, 0);
 }
