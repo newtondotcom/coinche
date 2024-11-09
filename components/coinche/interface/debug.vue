e5361db5064a652a65ac66a730e5680d620a2ff2
newtondotcom <asphalt9fr@gmail.com>
2024-11-09 16:12:17 +0100

    chore : more displays in debug

--- components/coinche/interface/debug.vue
+++ components/coinche/interface/debug.vue
@@ -1,13 +1,13 @@
 <template>
-    <div class="absolute bottom-0 right-4 bg-red-500">
-        Pli number : {{ storeGame.pliNumber }}
+    <div class="absolute bottom-0 right-4 bg-red-500 font-semibold">
+        Pli number : {{ storeGame.pli_number }}
         <br />
-        Current player id : {{ storeGame.currentPlayerId }}
+        Current player id : {{ storeGame.current_player_id }}
+        <br />
+        Deck length : {{ storeGame.deck.length }}
     </div>
 </template>
 
 <script setup lang="ts">
     const storeGame = useGameStore();
-    const storePlayers = usePlayersStore();
-    const storeAbout = useAboutStore();
 </script>
