1e2eab4fdf433868f648267ed1abadc6b649a5a9
newtondotcom <asphalt9fr@gmail.com>
2024-11-09 19:50:18 +0100

    chore : add datas to debug

--- components/coinche/interface/debug.vue
+++ components/coinche/interface/debug.vue
@@ -1,13 +1,22 @@
 <template>
-    <div class="absolute bottom-0 right-4 bg-red-500 font-semibold">
+    <div class="absolute bottom-0 right-4 bg-red-500 font-semibold z-20">
         Pli number : {{ storeGame.pli_number }}
         <br />
         Current player id : {{ storeGame.current_player_id }}
         <br />
         Deck length : {{ storeGame.deck.length }}
+        <br />
+
+        Distributing : {{ storeAbout.timeDistrib }}
+        <br />
+        Announcing : {{ storeAbout.timeToAnnonce }}
+        <br />
+        Playing : {{ storeGame.current_player_id }}
     </div>
 </template>
 
 <script setup lang="ts">
     const storeGame = useGameStore();
+    const storePlayers = usePlayersStore();
+    const storeAbout = useAboutStore();
 </script>
