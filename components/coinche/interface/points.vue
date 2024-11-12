3f8ac65a739b8b8a3fc702980ec8e3bf0ae24939
newtondotcom <asphalt9fr@gmail.com>
2024-11-12 09:39:40 +0100

    style : flex col for points

--- components/coinche/interface/points.vue
+++ components/coinche/interface/points.vue
@@ -1,91 +1,93 @@
 <template>
-    <Card class="w-[250px] mx-2 my-2">
-        <CardHeader>
-            <CardTitle>Points de la partie</CardTitle>
-        </CardHeader>
-        <CardContent class="grid gap-6">
-            <div class="flex items-center justify-between space-x-4">
-                <div class="flex items-center space-x-4">
-                    <Avatar>
-                        <AvatarFallback>
-                            {{ storeGame.team1_point_current_pli }}
-                        </AvatarFallback>
-                    </Avatar>
-                    <div>
-                        <p class="text-sm font-medium leading-none">
-                            {{ storePlayers.players[0].surname }} X
-                            {{ storePlayers.players[2].surname }}
-                        </p>
-                        <p class="text-sm text-muted-foreground">
-                            {{ storePlayers.players[0].classement }} ème /
-                            {{ storePlayers.players[2].classement }} ème
-                        </p>
+    <div class="flex flex-col">
+        <Card class="w-[250px] mx-2 my-2">
+            <CardHeader>
+                <CardTitle>Points de la partie</CardTitle>
+            </CardHeader>
+            <CardContent class="grid gap-6">
+                <div class="flex items-center justify-between space-x-4">
+                    <div class="flex items-center space-x-4">
+                        <Avatar>
+                            <AvatarFallback>
+                                {{ storeGame.team1_point_current_game }}
+                            </AvatarFallback>
+                        </Avatar>
+                        <div>
+                            <p class="text-sm font-medium leading-none">
+                                {{ storePlayers.players[0].surname }} X
+                                {{ storePlayers.players[2].surname }}
+                            </p>
+                            <p class="text-sm text-muted-foreground">
+                                {{ storePlayers.players[0].classement }} ème /
+                                {{ storePlayers.players[2].classement }} ème
+                            </p>
+                        </div>
                     </div>
                 </div>
-            </div>
-            <div class="flex items-center justify-between space-x-4">
-                <div class="flex items-center space-x-4">
-                    <Avatar>
-                        <AvatarFallback>
-                            {{ storeGame.team2_point_current_pli }}
-                        </AvatarFallback>
-                    </Avatar>
-                    <div>
-                        <p class="text-sm font-medium leading-none">
-                            {{ storePlayers.players[1].surname }} X
-                            {{ storePlayers.players[3].surname }}
-                        </p>
-                        <p class="text-sm text-muted-foreground">
-                            {{ storePlayers.players[1].classement }} ème /
-                            {{ storePlayers.players[3].classement }} ème
-                        </p>
+                <div class="flex items-center justify-between space-x-4">
+                    <div class="flex items-center space-x-4">
+                        <Avatar>
+                            <AvatarFallback>
+                                {{ storeGame.team2_point_current_game }}
+                            </AvatarFallback>
+                        </Avatar>
+                        <div>
+                            <p class="text-sm font-medium leading-none">
+                                {{ storePlayers.players[1].surname }} X
+                                {{ storePlayers.players[3].surname }}
+                            </p>
+                            <p class="text-sm text-muted-foreground">
+                                {{ storePlayers.players[1].classement }} ème /
+                                {{ storePlayers.players[3].classement }} ème
+                            </p>
+                        </div>
                     </div>
                 </div>
-            </div>
-        </CardContent>
-    </Card>
+            </CardContent>
+        </Card>
 
-    <Card class="w-[250px] mx-2 my-2">
-        <CardHeader>
-            <CardTitle>Points globaux</CardTitle>
-        </CardHeader>
-        <CardContent class="grid gap-6">
-            <div class="flex items-center justify-between space-x-4">
-                <div class="flex items-center space-x-4">
-                    <Avatar>
-                        <AvatarFallback>{{ storePlayers.team1[0].score }}</AvatarFallback>
-                    </Avatar>
-                    <div>
-                        <p class="text-sm font-medium leading-none">
-                            {{ storePlayers.players[0].surname }} X
-                            {{ storePlayers.players[2].surname }}
-                        </p>
-                        <p class="text-sm text-muted-foreground">
-                            {{ storePlayers.players[0].classement }} ème /
-                            {{ storePlayers.players[2].classement }} ème
-                        </p>
+        <Card class="w-[250px] mx-2 my-2">
+            <CardHeader>
+                <CardTitle>Points globaux</CardTitle>
+            </CardHeader>
+            <CardContent class="grid gap-6">
+                <div class="flex items-center justify-between space-x-4">
+                    <div class="flex items-center space-x-4">
+                        <Avatar>
+                            <AvatarFallback>{{ storePlayers.team1[0].score }}</AvatarFallback>
+                        </Avatar>
+                        <div>
+                            <p class="text-sm font-medium leading-none">
+                                {{ storePlayers.players[0].surname }} X
+                                {{ storePlayers.players[2].surname }}
+                            </p>
+                            <p class="text-sm text-muted-foreground">
+                                {{ storePlayers.players[0].classement }} ème /
+                                {{ storePlayers.players[2].classement }} ème
+                            </p>
+                        </div>
                     </div>
                 </div>
-            </div>
-            <div class="flex items-center justify-between space-x-4">
-                <div class="flex items-center space-x-4">
-                    <Avatar>
-                        <AvatarFallback>{{ storePlayers.team1[1].score }}</AvatarFallback>
-                    </Avatar>
-                    <div>
-                        <p class="text-sm font-medium leading-none">
-                            {{ storePlayers.players[1].surname }} X
-                            {{ storePlayers.players[3].surname }}
-                        </p>
-                        <p class="text-sm text-muted-foreground">
-                            {{ storePlayers.players[1].classement }} ème /
-                            {{ storePlayers.players[3].classement }} ème
-                        </p>
+                <div class="flex items-center justify-between space-x-4">
+                    <div class="flex items-center space-x-4">
+                        <Avatar>
+                            <AvatarFallback>{{ storePlayers.team1[1].score }}</AvatarFallback>
+                        </Avatar>
+                        <div>
+                            <p class="text-sm font-medium leading-none">
+                                {{ storePlayers.players[1].surname }} X
+                                {{ storePlayers.players[3].surname }}
+                            </p>
+                            <p class="text-sm text-muted-foreground">
+                                {{ storePlayers.players[1].classement }} ème /
+                                {{ storePlayers.players[3].classement }} ème
+                            </p>
+                        </div>
                     </div>
                 </div>
-            </div>
-        </CardContent>
-    </Card>
+            </CardContent>
+        </Card>
+    </div>
 </template>
 
 <script setup lang="ts">
