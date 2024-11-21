b1c32aba1fbb83a1f076b7d461d99104980c553c
newtondotcom <asphalt9fr@gmail.com>
2024-11-21 19:13:02 +0100

    fix : check for pli completion

--- ressources/master/src/listener/play.ts
+++ ressources/master/src/listener/play.ts
@@ -2,6 +2,7 @@ import { deformatCarteToPlay, type EventShared } from "@coinche/shared";
 import Master from "../game";
 import { setNextPlayerTurn } from "../utils";
 import { closePli } from "../emitter/close_pli";
+import logger from "../logger";
 
 export default async function translatePlay(event: EventShared) {
   const def = deformatCarteToPlay(event.value as string);
@@ -9,10 +10,10 @@ export default async function translatePlay(event: EventShared) {
   const pli_number = def.pli_number;
   const playerId = event.playerId;
   Master.getInstance(event.gameId).addPlay(card, playerId);
-  setNextPlayerTurn(playerId);
-
+  setNextPlayerTurn(playerId, event.gameId);
   // check if end of pli
-  if (Master.getInstance(event.gameId).getLastRound().pli.length === 4) {
+  if (Master.getInstance(event.gameId).getLastPli().plays.length === 4) {
+    logger.info("End of pli");
     await closePli(event.gameId);
   }
   return;
