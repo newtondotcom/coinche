9d42a07bff52c6be84dc7fb2544df66e624d8026
newtondotcom <asphalt9fr@gmail.com>
2024-11-22 01:33:30 +0100

    fix : set player can play in store

--- ressources/game/lib/listener/can.ts
+++ ressources/game/lib/listener/can.ts
@@ -2,15 +2,19 @@ import type { EventShared } from '@coinche/shared';
 
 export async function translateCanPlay(event: EventShared) {
     const storeAbout = useAboutStore();
+    const storeGame = useGameStore();
     if (event.value === storeAbout.myId) {
         console.log('can_play', event);
         storeAbout.setCanPlay(true);
     }
+    storeGame.setCurrentPlayerId(event.value as string);
 }
 export async function translateCanAnnonce(event: EventShared) {
     const storeAbout = useAboutStore();
+    const storeGame = useGameStore();
     if (event.value === storeAbout.myId) {
         console.log('can_annonce', event);
         storeAbout.setCanAnnonce(true);
     }
+    storeGame.setCurrentPlayerId(event.value as string);
 }
