3e4348fac027a45f9a00848aa1b82fc30eda714b
newtondotcom <asphalt9fr@gmail.com>
2024-11-26 15:44:05 +0100

    feat : start to consider auth with churros

---
+++ ressources/game/components/auth.vue
@@ -0,0 +1,8 @@
+<template>
+    <div class="flex flex-row">
+        <Button>Connexion avec Churros</Button>
+        <Button>Déconnexion</Button>
+    </div>
+</template>
+
+<script setup lang="ts"></script>
