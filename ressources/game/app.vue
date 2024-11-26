e4e721c29347d6829759e1babb5eda82280ee81c
newtondotcom <asphalt9fr@gmail.com>
2024-11-26 15:43:34 +0100

    style : changing header

--- ressources/game/app.vue
+++ ressources/game/app.vue
@@ -1,12 +1,10 @@
 <template>
     <div class="bg-secondary min-h-screen">
         <header class="px-2 py-2 flex flex-row justify-between">
-            <h1
-                class="bg-opacity-50 bg-gradient-to-b from-neutral-400 to-primary bg-clip-text text-center text-3xl font-bold text-transparent dark:from-neutral-50 dark:to-neutral-400 md:text-4xl cursor-pointer"
-            >
-                Coinche.xyz
+            <h1 class="text-primary text-center text-3xl font-bold md:text-4xl cursor-pointer">
+                coinche.n7
             </h1>
-            <div>Classement</div>
+            <h2 class="text-center">Classement</h2>
             <CoincheInterfaceColorMode />
         </header>
         <NuxtPage />
