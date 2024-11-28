1a8a5d50f0e2fe791bd4b0ccdfdf909b37b294c6
newtondotcom <asphalt9fr@gmail.com>
2024-11-28 17:43:55 +0100

    feat : function to recover the player surname

--- ressources/game/lib/utils/miscs.ts
+++ ressources/game/lib/utils/miscs.ts
@@ -1,5 +1,13 @@
+import type {PlayerId } from "@coinche/shared";
+
 export function assertPliNumber(n1: number, n2: number) {
     if (n1 !== n2) {
         console.error(`Pli number mismatch: ${n1} !== ${n2}`);
     }
+}
+
+export function findPlayerName(playerId: PlayerId) {
+    const storePlayers = usePlayersStore();
+    const players = storePlayers.players;
+    return players.find((p) => p.id === playerId)?.surname;
 }
