9afb5345bd5c0df6fee3e3d9e8f54af116b9e198
newtondotcom <asphalt9fr@gmail.com>
2024-12-03 10:28:54 +0100

    feat(game): 404 page requiring authentification

---
+++ ressources/game/pages/404.vue
@@ -0,0 +1,10 @@
+<template>
+    <div class="flex flex-col justify-center items-center min-h-screen">
+        <h1
+            class="mb-20 text-5xl font-bold bg-clip-text text-transparent drop-shadow-2xl bg-gradient-to-b from-primary/80 to-primary/40"
+        >
+            Cette page nécessite d'être authentifié
+        </h1>
+        <Auth />
+    </div>
+</template>
