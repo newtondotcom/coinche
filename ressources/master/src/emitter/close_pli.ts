9cdc536bfdd30785bd09155e69cb0bd3094acc49
newtondotcom <asphalt9fr@gmail.com>
2024-12-03 11:50:26 +0100

    feat(game): add game finished record and upadte mmr points

--- ressources/master/src/emitter/close_pli.ts
+++ ressources/master/src/emitter/close_pli.ts
@@ -1,116 +1,109 @@
-import { formatTeam, type IPlay, type IPlayer } from "@coinche/shared";
-import Master from "../game";
-import genIdCuid from "@coinche/shared/src/gen_id";
-import supabase from "../supabase";
-import { emitPoints } from "./points";
-import { emitEndRound } from "./end_round";
-import { emitEndGame } from "./end_game";
-import { startPli } from "./start_pli";
-import { emitRoundStarting } from "./start_round";
+import { formatTeam } from '@coinche/shared';
+import genIdCuid from '@coinche/shared/src/gen_id';
+import type { IPlay, IPlayer } from '@coinche/shared';
+
+import Master from '../game';
+import supabase from '../supabase';
+import { distributeRankingPoints, emitEndGame } from './end_game';
+import { emitEndRound } from './end_round';
+import { emitPoints } from './points';
+import { startPli } from './start_pli';
+import { emitRoundStarting } from './start_round';
 
 export async function closePli(gameId: string) {
-  const game = Master.getInstance(gameId).game;
-  const lastPli = Master.getInstance(gameId).getLastPli();
-  // find the winner
-  const pastPlis: IPlay[] = lastPli.plays;
-  const winnerPlayerId = findWinner(pastPlis, gameId);
-  const myIndex = game.players.findIndex(
-    (player: IPlayer) => player.id === winnerPlayerId,
-  );
-  const teamMatePlayerId = game.players[(myIndex + 2) % 4].id;
-  await supabase.from("Events").insert([
-    {
-      id: await genIdCuid(),
-      type: "win_pli",
-      playerId: "master",
-      gameId: gameId,
-      value: formatTeam(winnerPlayerId, teamMatePlayerId),
-    },
-  ]);
-  let score = pastPlis.reduce((acc, pli) => acc + pli.card.valueNum, 0);
-  if (game.deck.length === 32) {
-    score += 10;
-  }
-  const scoreTeam1 = Master.getInstance(gameId).isTeam1(winnerPlayerId)
-    ? score
-    : 0;
-  const scoreTeam2 = scoreTeam1 === 0 ? score : 0;
-  Master.getInstance(gameId).getLastRound().team1_point_current_game +=
-    scoreTeam1;
-  Master.getInstance(gameId).getLastRound().team2_point_current_game +=
-    scoreTeam2;
-  await emitPoints(scoreTeam1, scoreTeam2, gameId);
-
-  // end of the round
-  if (game.deck.length === 32) {
-    await emitEndRound(gameId);
-    // end of the game
-    const scoreToReach = 1000;
-    if (game.team1_score >= scoreToReach || game.team2_score >= scoreToReach) {
-      await emitEndGame(winnerPlayerId, teamMatePlayerId, gameId);
-    } else {
-      // next round if not goal score is reached
-      // update the db :
-      // fetch the last player starting id
-      const playerId = await fetchLastPliPlayerWinningId(gameId);
-      // emit the game starting event
-      await emitRoundStarting(gameId, playerId);
+    const game = Master.getInstance(gameId).game;
+    const lastPli = Master.getInstance(gameId).getLastPli();
+    // find the winner
+    const pastPlis: IPlay[] = lastPli.plays;
+    const winnerPlayerId = findWinner(pastPlis, gameId);
+    const myIndex = game.players.findIndex((player: IPlayer) => player.id === winnerPlayerId);
+    const teamMatePlayerId = game.players[(myIndex + 2) % 4].id;
+    await supabase.from('Events').insert([
+        {
+            id: await genIdCuid(),
+            type: 'win_pli',
+            playerId: 'master',
+            gameId: gameId,
+            value: formatTeam(winnerPlayerId, teamMatePlayerId),
+        },
+    ]);
+    let score = pastPlis.reduce((acc, pli) => acc + pli.card.valueNum, 0);
+    if (game.deck.length === 32) {
+        score += 10;
     }
-  } else {
-    // next pli
-    Master.getInstance(gameId).addPli(winnerPlayerId);
-    await startPli(gameId);
-  }
+    const scoreTeam1 = Master.getInstance(gameId).isTeam1(winnerPlayerId) ? score : 0;
+    const scoreTeam2 = scoreTeam1 === 0 ? score : 0;
+    Master.getInstance(gameId).getLastRound().team1_point_current_game += scoreTeam1;
+    Master.getInstance(gameId).getLastRound().team2_point_current_game += scoreTeam2;
+    await emitPoints(scoreTeam1, scoreTeam2, gameId);
 
-  return;
+    // end of the round
+    if (game.deck.length === 32) {
+        await emitEndRound(gameId);
+        // end of the game
+        const scoreToReach = 1000;
+        if (game.team1_score >= scoreToReach || game.team2_score >= scoreToReach) {
+            await emitEndGame(winnerPlayerId, teamMatePlayerId, gameId);
+            const playersIds = game.players.map((player) => player.id);
+            await distributeRankingPoints(playersIds, gameId, scoreTeam1, scoreTeam2);
+        } else {
+            // next round if not goal score is reached
+            // update the db :
+            // fetch the last player starting id
+            const playerId = await fetchLastPliPlayerWinningId(gameId);
+            // emit the game starting event
+            await emitRoundStarting(gameId, playerId);
+        }
+    } else {
+        // next pli
+        Master.getInstance(gameId).addPli(winnerPlayerId);
+        await startPli(gameId);
+    }
+
+    return;
 }
 
 function findWinner(lastPliEvents: IPlay[], gameId: string) {
-  const atout = Master.getInstance(gameId).getLastRound().last_annonce.suite;
-  if (lastPliEvents.some((pli) => pli.card.suite === atout)) {
-    // atout is played
-    const atoutCards = lastPliEvents.filter((pli) => pli.card.suite === atout);
-    const highestAtout = atoutCards.reduce((acc, card) => {
-      if (card.card.valueNum > acc.card.valueNum) {
-        return card;
-      }
-      return acc;
-    });
-    return highestAtout.playerId;
-  } else {
-    // no atout played
-    const firstSuite = lastPliEvents[0].card.suite;
-    const sameSuite = lastPliEvents.filter(
-      (pli) => pli.card.suite === firstSuite,
-    );
-    const highestSameSuite = sameSuite.reduce((acc, card) => {
-      if (card.card.valueNum > acc.card.valueNum) {
-        return card;
-      }
-      return acc;
-    });
-    return highestSameSuite.playerId;
-  }
+    const atout = Master.getInstance(gameId).getLastRound().last_annonce.suite;
+    if (lastPliEvents.some((pli) => pli.card.suite === atout)) {
+        // atout is played
+        const atoutCards = lastPliEvents.filter((pli) => pli.card.suite === atout);
+        const highestAtout = atoutCards.reduce((acc, card) => {
+            if (card.card.valueNum > acc.card.valueNum) {
+                return card;
+            }
+            return acc;
+        });
+        return highestAtout.playerId;
+    } else {
+        // no atout played
+        const firstSuite = lastPliEvents[0].card.suite;
+        const sameSuite = lastPliEvents.filter((pli) => pli.card.suite === firstSuite);
+        const highestSameSuite = sameSuite.reduce((acc, card) => {
+            if (card.card.valueNum > acc.card.valueNum) {
+                return card;
+            }
+            return acc;
+        });
+        return highestSameSuite.playerId;
+    }
 }
 
-export async function fetchLastPliPlayerWinningId(
-  gameId: string,
-): Promise<string> {
-  const { data: events, error } = await supabase
-    .from("Events")
-    .select("value")
-    .eq("type", "start_round")
-    .eq("gameId", gameId);
-  if (error) {
-    console.error(error);
-    return " ";
-  }
-  const playerStartedId = events[0].value;
-  const playerStartedIndex = Master.getInstance(gameId).game.players.findIndex(
-    (player) => player.id === playerStartedId,
-  );
-  const playerStartingIndex = (playerStartedIndex + 1) % 4;
-  const playerId =
-    Master.getInstance(gameId).game.players[playerStartingIndex].id;
-  return playerId;
+export async function fetchLastPliPlayerWinningId(gameId: string): Promise<string> {
+    const { data: events, error } = await supabase
+        .from('Events')
+        .select('value')
+        .eq('type', 'start_round')
+        .eq('gameId', gameId);
+    if (error) {
+        console.error(error);
+        return ' ';
+    }
+    const playerStartedId = events[0].value;
+    const playerStartedIndex = Master.getInstance(gameId).game.players.findIndex(
+        (player) => player.id === playerStartedId,
+    );
+    const playerStartingIndex = (playerStartedIndex + 1) % 4;
+    const playerId = Master.getInstance(gameId).game.players[playerStartingIndex].id;
+    return playerId;
 }
--- ressources/master/src/emitter/end_game.ts
+++ ressources/master/src/emitter/end_game.ts
@@ -1,19 +1,103 @@
-import genIdCuid from "@coinche/shared/src/gen_id";
-import supabase from "../supabase";
-import { formatTeam } from "@coinche/shared";
+import { formatTeam } from '@coinche/shared';
+import genIdCuid from '@coinche/shared/src/gen_id';
+import type { PlayerId } from '@coinche/shared';
+
+import { addPointsTo } from '../points';
+import supabase from '../supabase';
 
 export async function emitEndGame(
-  winnerPlayerId: string,
-  teamMatePlayerId: string,
-  gameId: string,
+    winnerPlayerId: string,
+    teamMatePlayerId: string,
+    gameId: string,
 ) {
-  await supabase.from("Events").insert([
-    {
-      id: await genIdCuid(),
-      type: "end_game",
-      playerId: "master",
-      gameId: gameId,
-      value: formatTeam(winnerPlayerId, teamMatePlayerId),
-    },
-  ]);
+    await supabase.from('Events').insert([
+        {
+            id: await genIdCuid(),
+            type: 'end_game',
+            playerId: 'master',
+            gameId: gameId,
+            value: formatTeam(winnerPlayerId, teamMatePlayerId),
+        },
+    ]);
+}
+
+export async function distributeRankingPoints(
+    players: PlayerId[],
+    gameId: string,
+    team1Score: number,
+    team2Score: number,
+) {
+    // store in db the finished game
+    await supabase.from('Game').insert([
+        {
+            gameId: gameId,
+            p1: players[0],
+            p2: players[1],
+            p3: players[2],
+            p4: players[3],
+            team1_score: team1Score,
+            team2_score: team2Score,
+        },
+    ]);
+
+    // distribute points
+
+    const { data, error } = await supabase.from('Points').select('*').in('playerId', players);
+    if (error || !data || data.length !== players.length) {
+        console.error('Error fetching points or missing points data for some players', error);
+        return;
+    }
+
+    // Mapping players to their points
+    const playerPoints = players.map(
+        (playerId) => data.find((d) => d.playerId === playerId)?.points,
+    );
+
+    if (playerPoints.includes(undefined)) {
+        console.error('Some players have missing points data.');
+        return;
+    }
+
+    const mmrT1 = (playerPoints[0] + playerPoints[2]) / 2;
+    const mmrT2 = (playerPoints[1] + playerPoints[3]) / 2;
+    const diffMMR = Math.abs(mmrT1 - mmrT2);
+    const team1Win = team1Score > team2Score;
+
+    // Points adjustment based on game result
+    const adjustPoints = (playerIdx: number, adjustment: number) =>
+        addPointsTo(adjustment, players[playerIdx]);
+
+    if (team1Win) {
+        if (mmrT1 < mmrT2) {
+            // TEAM 1
+            adjustPoints(0, 10 + diffMMR / 20);
+            adjustPoints(2, 10 + diffMMR / 20);
+            // TEAM 2
+            adjustPoints(3, -10 - diffMMR / 20);
+            adjustPoints(1, -10 - diffMMR / 20);
+        } else {
+            // TEAM 1
+            adjustPoints(0, 10 - diffMMR / 20);
+            adjustPoints(2, 10 - diffMMR / 20);
+            // TEAM 2
+            adjustPoints(3, -10 + diffMMR / 20);
+            adjustPoints(1, -10 + diffMMR / 20);
+        }
+    } else {
+        if (mmrT1 < mmrT2) {
+            // TEAM 1
+            adjustPoints(0, -10 + diffMMR / 20);
+            adjustPoints(2, -10 + diffMMR / 20);
+            // TEAM 2
+            adjustPoints(3, 10 - diffMMR / 20);
+            adjustPoints(1, 10 - diffMMR / 20);
+        } else {
+            // TEAM 1
+            adjustPoints(0, -10 - diffMMR / 20);
+            adjustPoints(2, -10 - diffMMR / 20);
+            // TEAM 2
+            adjustPoints(3, 10 + diffMMR / 20);
+            adjustPoints(1, 10 + diffMMR / 20);
+        }
+    }
 }
