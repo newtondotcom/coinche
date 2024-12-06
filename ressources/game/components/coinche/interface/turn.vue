286f84dc4ba5cb79f9f3480c2fd479da90cbc866
newtondotcom <asphalt9fr@gmail.com>
2024-12-06 19:37:15 +0100

    fix(game): display turn based on storeAbout.myId

--- ressources/game/components/coinche/interface/turn.vue
+++ ressources/game/components/coinche/interface/turn.vue
@@ -7,51 +7,70 @@
                 <div class="flex justify-center items-center">
                     <Badge
                         :variant="
-                            playerIdTurn === storePlayers.players[0].id ? 'default' : 'secondary'
+                            playerIdTurn ===
+                            storePlayers.players[(indexPlayers + 2) % lengthPlayers].id
+                                ? 'default'
+                                : 'secondary'
                         "
                         class="mx-1"
                     >
-                        {{ storePlayers.players[0].hands.length }}
+                        {{ storePlayers.players[(indexPlayers + 2) % lengthPlayers].hands.length }}
                     </Badge>
-                    <span class="font-semibold mx-1">{{ storePlayers.players[0].id }}</span>
+                    <span class="font-semibold mx-1">
+                        {{ storePlayers.players[(indexPlayers + 2) % lengthPlayers].id }}
+                    </span>
                 </div>
                 <div />
 
                 <div class="flex justify-center items-center">
                     <Badge
                         :variant="
-                            playerIdTurn === storePlayers.players[1].id ? 'default' : 'secondary'
+                            playerIdTurn ===
+                            storePlayers.players[(indexPlayers + 1) % lengthPlayers].id
+                                ? 'default'
+                                : 'secondary'
                         "
                         class="mx-1"
                     >
-                        {{ storePlayers.players[1].hands.length }}
+                        {{ storePlayers.players[(indexPlayers + 1) % lengthPlayers].hands.length }}
                     </Badge>
-                    <span class="font-semibold mx-1">{{ storePlayers.players[1].id }}</span>
+                    <span class="font-semibold mx-1">
+                        {{ storePlayers.players[(indexPlayers + 1) % lengthPlayers].id }}
+                    </span>
                 </div>
                 <div />
                 <div class="flex justify-center items-center">
                     <Badge
                         :variant="
-                            playerIdTurn === storePlayers.players[2].id ? 'default' : 'secondary'
+                            playerIdTurn ===
+                            storePlayers.players[(indexPlayers + 3) % lengthPlayers].id
+                                ? 'default'
+                                : 'secondary'
                         "
                         class="mx-1"
                     >
-                        {{ storePlayers.players[2].hands.length }}
+                        {{ storePlayers.players[(indexPlayers + 3) % lengthPlayers].hands.length }}
                     </Badge>
-                    <span class="font-semibold mx-1">{{ storePlayers.players[2].id }}</span>
+                    <span class="font-semibold mx-1">
+                        {{ storePlayers.players[(indexPlayers + 3) % lengthPlayers].id }}
+                    </span>
                 </div>
 
                 <div />
                 <div class="flex justify-center items-center">
                     <Badge
                         :variant="
-                            playerIdTurn === storePlayers.players[3].id ? 'default' : 'secondary'
+                            playerIdTurn === storePlayers.players[indexPlayers].id
+                                ? 'default'
+                                : 'secondary'
                         "
                         class="mx-1"
                     >
-                        {{ storePlayers.players[3].hands.length }}
+                        {{ storePlayers.players[indexPlayers].hands.length }}
                     </Badge>
-                    <span class="font-semibold mx-1">{{ storePlayers.players[3].id }}</span>
+                    <span class="font-semibold mx-1">
+                        {{ storePlayers.players[indexPlayers].id }}
+                    </span>
                 </div>
                 <div />
             </div>
@@ -63,6 +82,11 @@
 <script setup lang="ts">
     const storePlayers = usePlayersStore();
     const storeGame = useGameStore();
+    const storeAbout = useAboutStore();
 
-    const playerIdTurn = computed(() => storeGame.current_player_id);
+    const playerIdTurn = computed<string>(() => storeGame.current_player_id);
+    const indexPlayers = computed<number>(() =>
+        storePlayers.players.findIndex((p) => p.id == storeAbout.myId),
+    );
+    const lengthPlayers = computed<number>(() => storePlayers.players.length);
 </script>
