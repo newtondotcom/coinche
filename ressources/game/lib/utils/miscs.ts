c5484d0a76675b8d32f930227d591393a65c0a1e
newtondotcom <asphalt9fr@gmail.com>
2024-12-07 17:55:12 +0100

    fix(game): delete env signaling dev

--- ressources/game/lib/utils/miscs.ts
+++ ressources/game/lib/utils/miscs.ts
@@ -3,6 +3,3 @@ export function assertPliNumber(n1: number, n2: number) {
         console.error(`Pli number mismatch: ${n1} !== ${n2}`);
     }
 }
-
-const config = useRuntimeConfig();
-export const devEnv = config.public.NODE_ENV !== 'production';
--- ressources/game/middleware/auth.global.ts
+++ ressources/game/middleware/auth.global.ts
@@ -1,8 +1,9 @@
 export default defineNuxtRouteMiddleware((to, _) => {
     const storeAbout = useAboutStore();
-    const allowedPaths = ['/', '/login', '/404', '/regles', '/partie'];
-    console.log('to', to.path);
-    if (!allowedPaths.includes(to.path) && !storeAbout.authentificated) {
+    const allowedPaths = ['/', '/login', '/404', '/regles'];
+    const config = useRuntimeConfig();
+    const devEnv = config.public.NODE_ENV !== 'production';
+    if (!devEnv || (!allowedPaths.includes(to.path) && !storeAbout.authentificated)) {
         if (to.path !== '/404') {
             return navigateTo('/404');
         }
