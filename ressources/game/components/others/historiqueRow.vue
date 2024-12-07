ea15922b78c83332aac2231829592ee127001aa7
newtondotcom <asphalt9fr@gmail.com>
2024-12-07 17:54:13 +0100

    feat(game): page with game history

---
+++ ressources/game/components/others/historiqueRow.vue
@@ -0,0 +1,38 @@
+<template>
+    <div
+        class="flex flex-row space-x-6 justify-center items-center border-2 px-4 py-2 rounded-lg my-2 w-[800px]"
+        :class="playerWin ? 'bg-green-200' : 'bg-red-200'"
+    >
+        <div class="flex flex-col text-xl">
+            <div class="font-semibold w-[300px]">{{ row.p1 }} & {{ row.p3 }}</div>
+            <div class="font-semibold w-[300px]">{{ row.team1_score }}</div>
+        </div>
+        <div class="flex flex-col text-2xl font-bold text-red-500">VS</div>
+        <div class="flex flex-col text-xl">
+            <div class="font-semibold w-[300px]">{{ row.p2 }} & {{ row.p4 }}</div>
+            <div class="font-semibold w-[300px]">{{ row.team2_score }}</div>
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+    import type { HistoriqueRow } from '@coinche/shared';
+
+    interface HistoriqueRowProps {
+        row: HistoriqueRow;
+    }
+    const props = defineProps<HistoriqueRowProps>();
+    const storeAbout = useAboutStore();
+
+    const playerTeam1 = computed(() => {
+        return props.row.p1 === storeAbout.myId || props.row.p3 === storeAbout.myId;
+    });
+
+    const team1Win = computed(() => {
+        return props.row.team1_score > props.row.team2_score;
+    });
+
+    const playerWin = computed(() => {
+        return (playerTeam1.value && team1Win.value) || (!playerTeam1.value && !team1Win.value);
+    });
+</script>
---
+++ ressources/game/pages/historique.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="flex flex-col justify-center items-center">
+        <h1
+            class="my-4 text-5xl font-bold bg-clip-text text-transparent drop-shadow-2xl bg-gradient-to-b from-primary/100 to-primary/40"
+        >
+            Historique
+        </h1>
+        <OthersHistoriqueRow v-for="row in historique" :key="row.gameId" :row="row" />
+    </div>
+</template>
+
+<script setup lang="ts">
+    import type { HistoriqueRow } from '@coinche/shared';
+
+    import { supabase } from '../lib/utils/listener';
+
+    const historique = ref<HistoriqueRow[]>([]);
+
+    const storeAbout = useAboutStore();
+
+    async function fetchHistorique() {
+        console.log(storeAbout.myId);
+        const { data, error } = await supabase
+            .from('Game')
+            .select()
+            .or(
+                `p1.eq.${storeAbout.myId},p2.eq.${storeAbout.myId},p3.eq.${storeAbout.myId},p4.eq.${storeAbout.myId}`,
+            );
+
+        if (error) throw error;
+        historique.value = data;
+    }
+
+    onMounted(() => {
+        fetchHistorique();
+    });
+</script>
