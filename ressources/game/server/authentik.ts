27f88e8025ef9190c2115deb8059a943acd8788f
newtondotcom <asphalt9fr@gmail.com>
2024-12-12 11:20:48 +0100

    feat(game): define authentik redirect based on env var

--- ressources/game/server/authentik.ts
+++ ressources/game/server/authentik.ts
@@ -3,11 +3,14 @@ import { Authentik } from 'arctic';
 const domain = 'auth.inpt.fr';
 const config = useRuntimeConfig();
 
+const redirectUrl =
+    config.NODE_ENV == 'production' ? 'https://coinche-game.vercel.app/' : 'http://localhost:3000/';
+
 const authentik = new Authentik(
     domain,
     config.CHURROS_CLIENT_ID,
     config.CHURROS_CLIENT_SECRET,
-    'http://localhost:3000/login',
+    redirectUrl + 'login',
 );
 
 export default authentik;
