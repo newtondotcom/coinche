36bf4cd117b803f708b8f3ba0b383d3353955629
newtondotcom <asphalt9fr@gmail.com>
2025-01-23 15:39:47 +0100

    chore: remove useless log

--- ressources/game/middleware/auth.global.ts
+++ ressources/game/middleware/auth.global.ts
@@ -5,10 +5,6 @@ export default defineNuxtRouteMiddleware((to, _) => {
     const allowedPaths = ['/', '/login', '/404', '/regles', '/api/alive'];
     const config = useRuntimeConfig();
     const devEnv = isDevEnv(config);
-
-    console.log('path out', to.path);
-
-    // Corrected logic: Redirect ONLY if NOT allowed AND NOT authenticated AND NOT devEnv
     if (!devEnv && !allowedPaths.includes(to.path) && !storeAbout.authentificated) {
         return navigateTo('/404');
     }
