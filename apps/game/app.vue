b2a73617a8765d18a2a2d277d48242375008c917
newtondotcom <asphalt9fr@gmail.com>
2025-06-18 22:24:51 +0200

    fix: sonner import

--- apps/game/app.vue
+++ apps/game/app.vue
@@ -51,9 +51,6 @@
 
 <script setup lang="ts">
     import { Toaster } from '@/components/ui/sonner';
-
-    import 'vue-sonner/style.css';
-
     import { isDevEnv } from '~/lib/utils/miscs';
 
     const config = useRuntimeConfig();
