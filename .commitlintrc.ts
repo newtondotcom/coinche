79079463a9ab1b18c6690634ce1bf3fee3cb5e94
newtondotcom <asphalt9fr@gmail.com>
2025-06-18 22:28:08 +0200

    chore: rm useless husky hooks

--- .commitlintrc.ts
+++
@@ -1,7 +0,0 @@
-export default {
-    extends: ['@commitlint/config-conventional'],
-    rules: {
-        'header-max-length': [1, 'always', 200],
-        'body-max-line-length': [1, 'always', Infinity],
-    },
-};
