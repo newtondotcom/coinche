c5462f243d6563f0a331600d75470699e62cb6c6
newtondotcom <asphalt9fr@gmail.com>
2025-06-27 14:22:28 +0200

    feat: make header a component + hide in it in "partie" page

--- apps/game/app.vue
+++ apps/game/app.vue
@@ -1,41 +1,6 @@
 <template>
   <div class="bg-secondary min-h-screen">
-    <header class="px-2 py-2 flex flex-row justify-between items-center">
-      <NuxtLink to="/">
-        <h1
-          class="text-primary text-center text-3xl font-bold mx-2 md:text-4xl cursor-pointer"
-        >
-          coinche.n7
-        </h1>
-      </NuxtLink>
-      <div class="flex flex-row space-x-10">
-        <NuxtLink v-if="loggedIn" to="/classement">
-          <h2
-            class="text-2xl font-semibold text-neutral-500 dark:text-neutral-200 flex items-center"
-          >
-            classement
-          </h2>
-        </NuxtLink>
-        <NuxtLink v-if="loggedIn" to="/historique">
-          <h2
-            class="text-2xl font-semibold text-neutral-500 dark:text-neutral-200 flex items-center"
-          >
-            historique
-          </h2>
-        </NuxtLink>
-        <NuxtLink to="/regles">
-          <h2
-            class="text-2xl font-semibold text-neutral-500 dark:text-neutral-200 flex items-center"
-          >
-            règles
-          </h2>
-        </NuxtLink>
-      </div>
-      <div class="flex flex-row">
-        <CoincheInterfaceColorMode />
-        <OthersAuth />
-      </div>
-    </header>
+    <OthersHeader />
     <Separator />
     <NuxtPage />
     <footer />
@@ -54,8 +19,6 @@ import { Toaster } from "@/components/ui/sonner";
 import { isDevEnv } from "@/shared/utils/miscs";
 
 const config = useRuntimeConfig();
-
-const {loggedIn} = useAuth()
 
 useSeoMeta({
   title: "Coinche.n7",
