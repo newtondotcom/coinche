09367b62780e44600c2819282c769caa8d9abf26
Robin Augereau <102476446+newtondotcom@users.noreply.github.com>
2025-07-29 13:30:37 +0000

    fix: correctly get username and image url

--- apps/game/server/utils/auth.ts
+++ apps/game/server/utils/auth.ts
@@ -10,6 +10,17 @@ export const auth = betterAuth({
     provider: "pg",
     schema: schema,
   }),
+
+  user: {
+    additionalFields: {
+      username: {
+        type: "string",
+        required: true,
+        defaultValue: "",
+        input: false,
+      }
+    },
+  },
   plugins: [
     genericOAuth({
       config: [
@@ -18,7 +29,42 @@ export const auth = betterAuth({
           clientId: config.CHURROS_CLIENT_ID,
           clientSecret: config.CHURROS_CLIENT_SECRET,
           authorizationUrl : config.CHURROS_AUTHORIZATION_URL,
-          tokenUrl : config.CHURROS_TOKEN_URL
+          tokenUrl : config.CHURROS_TOKEN_URL,
+          scopes : ['openid', 'profile', 'preferred_username',"email","churros:profile"],
+          async getUserInfo(tokens) {
+                const userInfoUrl = process.env.CHURROS_USER_INFO;
+                if (!userInfoUrl) {
+                  throw new Error("CHURROS_USER_INFO environment variable is not set");
+                }
+
+                const response = await fetch(userInfoUrl, {
+                  headers: {
+                    "Authorization": `Bearer ${tokens.accessToken}`,
+                    "Content-Type": "application/json",
+                  },
+                });
+
+                if (!response.ok) {
+                  throw new Error(`Failed to fetch user info: ${response.status} ${response.statusText}`);
+                }
+
+                const userInfo = await response.json();
+
+                console.info("User info received", { userInfo });
+
+                const userId = userInfo.sub || "";
+
+                return {
+                  id: userId,
+                  name: userInfo.fullName ||"",
+                  username: userInfo.uid || userInfo.nickname || "",
+                  image: userInfo.pictureURL || null,
+                  createdAt: new Date(),
+                  updatedAt: new Date(),
+                  email : userInfo.email,
+                  emailVerified : userInfo.email_verified,
+                };
+              },
         },
       ],
     }),
