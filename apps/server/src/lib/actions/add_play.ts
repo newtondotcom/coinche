31b8f6ee896f170b9ce2eb02e99b89568c1d39c8
newtondotcom <asphalt9fr@gmail.com>
2025-08-06 18:21:50 +0200

    fix(back): remove a card from player hand once it is played

--- apps/server/src/lib/actions/add_play.ts
+++ apps/server/src/lib/actions/add_play.ts
@@ -8,6 +8,7 @@ export default function  addPlay(card: ICard, playerId: string, gameId: string):
     const currentPli = currentRound.plis[currentRound.plis.length - 1]; 
     currentPli.plays.push({ card, playerId });
     controllerInstance.state.deck.push(card);
+    controllerInstance.state.players.find((player) => player.id === playerId)!.hands.splice(controllerInstance.state.players.find((player) => player.id === playerId)!.hands.indexOf(card), 1);
     logger.info(`Player ${playerId} played ${card.suite} of ${card.value}`);
     controllerInstance.sendState();
 }
