c9b670410780efc12596fb15c47b8bea1908c9ad
newtondotcom <asphalt9fr@gmail.com>
2025-08-07 22:21:47 +0200

    fix(back): correctly remove played card

--- apps/server/src/lib/actions/add_play.ts
+++ apps/server/src/lib/actions/add_play.ts
@@ -8,7 +8,13 @@ export default function  addPlay(card: ICard, playerId: string, gameId: string):
     const currentPli = currentRound.plis[currentRound.plis.length - 1]; 
     currentPli.plays.push({ card, playerId });
     controllerInstance.state.deck.push(card);
-    controllerInstance.state.players.find((player) => player.id === playerId)!.hands.splice(controllerInstance.state.players.find((player) => player.id === playerId)!.hands.indexOf(card), 1);
+    const player = controllerInstance.state.players.find((player) => player.id === playerId)!;
+    const cardIndex = player.hands.findIndex((hand) => hand.suite === card.suite && hand.value === card.value);
+    if (cardIndex !== -1) {
+        player.hands.splice(cardIndex, 1);
+    } else {
+        logger.error(`Player ${playerId} does not have the card ${card.suite} of ${card.value}`);
+    }
     logger.info(`Player ${playerId} played ${card.suite} of ${card.value}`);
     controllerInstance.sendState();
 }
