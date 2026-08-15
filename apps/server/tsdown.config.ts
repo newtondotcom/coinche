6519fdaea9db6433ef16fd30bf6263a964686e12
newtondotcom <asphalt9fr@gmail.com>
2026-08-15 12:18:43 +0200

    chore: update deps

--- apps/server/tsdown.config.ts
+++ apps/server/tsdown.config.ts
@@ -5,5 +5,7 @@ export default defineConfig({
   format: "esm",
   outDir: "./dist",
   clean: true,
-  noExternal: [/@coinche-reborn\/.*/],
+  deps: {
+    alwaysBundle: [/@coinche-reborn\/.*/],
+  },
 });
