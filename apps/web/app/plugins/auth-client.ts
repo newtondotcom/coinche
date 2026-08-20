1d8a53ca4e41520500a16a14466dd0a3caa922cf
newtondotcom <asphalt9fr@gmail.com>
2026-08-20 19:16:54 +0200

    chore: bump deps

--- apps/web/app/plugins/auth-client.ts
+++ apps/web/app/plugins/auth-client.ts
@@ -1,12 +1,12 @@
 import { createAuthClient } from "better-auth/vue";
-import { genericOAuthClient, inferAdditionalFields } from "better-auth/client/plugins";
+import { inferAdditionalFields } from "better-auth/client/plugins";
 import type { Auth } from "@coinche-reborn/auth/type";
 import { env } from "@coinche-reborn/env/web";
 
 export default defineNuxtPlugin(() => {
   const authClient = createAuthClient({
     baseURL: env.NUXT_PUBLIC_API_URL,
-    plugins: [genericOAuthClient(), inferAdditionalFields<Auth>()],
+    plugins: [inferAdditionalFields<Auth>()],
   });
 
   return {
